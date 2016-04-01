3949c8ba24ea5bd6bd07da1d8e0b316bb8d9078e
falkTX <falktx@gmail.com>
2016-04-01 17:39:07 +0200

    Add python-mpmath host package, needed for mod-pitchshifter

---
+++ plugins-dep/package/python-mpmath/python-mpmath.mk
@@ -0,0 +1,11 @@
+######################################
+#
+# python-mpmath
+#
+######################################
+HOST_PYTHON_MPMATH_SOURCE = mpmath-0.19.tar.gz
+HOST_PYTHON_MPMATH_SITE = https://pypi.python.org/packages/source/m/mpmath
+HOST_PYTHON_MPMATH_VERSION = 0.19
+HOST_PYTHON_MPMATH_SETUP_TYPE = setuptools
+
+$(eval $(host-python-package))
