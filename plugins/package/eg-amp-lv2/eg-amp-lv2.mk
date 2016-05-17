7c11df047b180e65fe3030530eaccf3a0abee935
falkTX <falktx@gmail.com>
2016-05-17 17:48:02 +0200

    Even better now :)

--- plugins/package/eg-amp-lv2/eg-amp-lv2.mk
+++ plugins/package/eg-amp-lv2/eg-amp-lv2.mk
@@ -5,11 +5,11 @@
 ######################################
 
 # where to find the source code - locally in this case
-EG_AMP_LV2_SITE_METHOD = file
-EG_AMP_LV2_SITE = .
-EG_AMP_LV2_SOURCE = .
+EG_AMP_LV2_SITE_METHOD = local
+EG_AMP_LV2_SITE = $($(PKG)_PKGDIR)/
 
 # even though this is a local build, we still need a version number
+# bump this number if you need to force a rebuild
 EG_AMP_LV2_VERSION = 1
 
 # dependencies (list of other buildroot packages, separated by space)
@@ -19,14 +19,8 @@ EG_AMP_LV2_DEPENDENCIES =
 EG_AMP_LV2_BUNDLES = eg-amp.lv2
 
 # call make with the current arguments and path. "$(@D)" is the build directory.
-EG_AMP_LV2_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
+EG_AMP_LV2_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/source
 
-
-# instead of extracting code tarball (which doesn't exist), copy the local contents to the build dir
-# this makes sure the build is always clean
-define EG_AMP_LV2_EXTRACT_CMDS
-	cp -r $($(PKG)_PKGDIR)/source/* $(@D)
-endef
 
 # build command
 define EG_AMP_LV2_BUILD_CMDS
