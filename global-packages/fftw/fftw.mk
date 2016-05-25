38baa7ffd4f3dd81865a0debf9199c77faa73a56
falkTX <falktx@gmail.com>
2016-05-25 10:52:30 +0200

    Update fftw

--- global-packages/fftw/fftw.mk
+++ global-packages/fftw/fftw.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-FFTW_VERSION = 3.3.5+git20160517
-FFTW_SITE = http://web1.moddevices.com/shared
+FFTW_VERSION = 3.3.5-2
+FFTW_SITE = https://github.com/falkTX/fftw3/releases/download/fftw-$(FFTW_VERSION)
 FFTW_SOURCE = fftw-$(FFTW_VERSION).tar.gz
 FFTW_LICENSE = GPLv2+
 FFTW_LICENSE_FILES = COPYING
@@ -15,8 +15,8 @@ FFTW_CONF_OPTS  = --enable-openmp
 FFTW_CONF_OPTS += --enable-threads
 FFTW_CONF_OPTS += --disable-doc
 FFTW_CONF_OPTS += --disable-debug
-FFTW_CONF_OPTS += --enable-generic-simd128
-FFTW_CONF_OPTS += --enable-generic-simd256
+# FFTW_CONF_OPTS += --enable-generic-simd128
+# FFTW_CONF_OPTS += --enable-generic-simd256
 ifdef BR2_x86_64
 FFTW_CONF_OPTS += --enable-sse2
 endif
--- plugins-dep/package/fftwf/fftwf.mk
+++ plugins-dep/package/fftwf/fftwf.mk
@@ -4,20 +4,22 @@
 #
 ################################################################################
 
-FFTWF_VERSION = 3.3.5+git20160517
-FFTWF_SITE = http://web1.moddevices.com/shared
+FFTWF_VERSION = 3.3.5-2
+FFTWF_SITE = https://github.com/falkTX/fftw3/releases/download/fftw-$(FFTWF_VERSION)
 FFTWF_SOURCE = fftw-$(FFTWF_VERSION).tar.gz
 FFTWF_LICENSE = GPLv2+
 FFTWF_LICENSE_FILES = COPYING
 FFTWF_INSTALL_STAGING = YES
+# FFTWF_DEPENDENCIES = ffmpeg
 
 FFTWF_CONF_OPTS  = --enable-openmp
 FFTWF_CONF_OPTS += --enable-threads
 FFTWF_CONF_OPTS += --enable-single
 FFTWF_CONF_OPTS += --disable-doc
 FFTWF_CONF_OPTS += --disable-debug
-FFTWF_CONF_OPTS += --enable-generic-simd128
-FFTWF_CONF_OPTS += --enable-generic-simd256
+# FFTWF_CONF_OPTS += --enable-ffmpeg
+# FFTWF_CONF_OPTS += --enable-generic-simd128
+# FFTWF_CONF_OPTS += --enable-generic-simd256
 ifdef BR2_arm
 FFTWF_CONF_OPTS += --enable-neon
 endif
@@ -27,6 +29,9 @@ FFTWF_CONF_OPTS += --enable-sse2
 endif
 
 FFTWF_CONF_OPTS += CFLAGS="$(TARGET_CFLAGS) -fstrict-aliasing" CXXFLAGS="$(TARGET_CXXFLAGS) -fstrict-aliasing"
+ifdef BR2_arm
+FFTWF_CONF_OPTS += NEON_CFLAGS="-mfpu=neon-vfpv4"
+endif
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
