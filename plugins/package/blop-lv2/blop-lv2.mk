e7630a61e5d4370ddcf4f44e2d001919e3badc26
falkTX <falktx@gmail.com>
2016-07-27 12:53:42 +0200

    Cleanup blop

--- plugins/package/blop-lv2/blop-lv2.mk
+++ plugins/package/blop-lv2/blop-lv2.mk
@@ -7,41 +7,16 @@
 BLOP_LV2_VERSION = 44ff27dd355e850b9865fa50e3f32d8e24619006
 BLOP_LV2_SITE = http://git.drobilla.net/blop.lv2.git
 BLOP_LV2_SITE_METHOD = git
-BLOP_LV2_DEPENDENCIES = host-blop-lv2 host-lv2 host-python
 BLOP_LV2_BUNDLES = blop.lv2
 
-BLOP_HOST_WAF   = $(HOST_MAKE_ENV)   $(HOST_CONFIGURE_OPTS)   $(HOST_DIR)/usr/bin/python ./waf
 BLOP_TARGET_WAF = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(HOST_DIR)/usr/bin/python ./waf
-
-BLOP_LV2_TMP_WAVEGEN = $(HOST_DIR)/blop-lv2-wavegen
-
-define HOST_BLOP_LV2_CONFIGURE_CMDS
-        (cd $(@D); $(BLOP_HOST_WAF) configure --prefix=/usr)
-endef
-
-define HOST_BLOP_LV2_BUILD_CMDS
-	(cd $(@D); $(BLOP_HOST_WAF) build -j $(PARALLEL_JOBS))
-	# copy wavegen for target build
-	cp $(@D)/build/src/wavegen $(BLOP_LV2_TMP_WAVEGEN)
-endef
 
 define BLOP_LV2_CONFIGURE_CMDS
 	(cd $(@D); $(BLOP_TARGET_WAF) configure --prefix=/usr)
 endef
 
 define BLOP_LV2_BUILD_CMDS
-	# initial build that fails
-	(cd $(@D); $(BLOP_TARGET_WAF) build -j $(PARALLEL_JOBS) || true)
-
-	# copy host wavegen
-	mkdir -p $(@D)/build/src
-	cp $(BLOP_LV2_TMP_WAVEGEN) $(@D)/build/src/wavegen
-
-	# now fully build
 	(cd $(@D); $(BLOP_TARGET_WAF) build -j $(PARALLEL_JOBS))
-
-	# cleanup
-	rm $(BLOP_LV2_TMP_WAVEGEN)
 endef
 
 define BLOP_LV2_INSTALL_TARGET_CMDS
@@ -50,4 +25,3 @@ define BLOP_LV2_INSTALL_TARGET_CMDS
 endef
 
 $(eval $(generic-package))
-$(eval $(host-generic-package))
