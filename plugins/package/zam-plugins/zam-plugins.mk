13c9999eed008bab92da147083e424e9a4f4ffa2
falkTX <falktx@gmail.com>
2016-07-27 12:51:11 +0200

    Cleanup to remove host side build

--- plugins/package/zam-plugins/zam-plugins.mk
+++ plugins/package/zam-plugins/zam-plugins.mk
@@ -6,13 +6,9 @@
 
 ZAM_PLUGINS_VERSION = 21ccac0c15e1e54d482ff0931b55975a7e2ac62e
 ZAM_PLUGINS_SITE = $(call github,zamaudio,zam-plugins,$(ZAM_PLUGINS_VERSION))
-ZAM_PLUGINS_DEPENDENCIES = host-zam-plugins
 ZAM_PLUGINS_BUNDLES = ZamAutoSat.lv2 ZaMaximX2.lv2 ZamComp.lv2 ZamCompX2.lv2 ZamDelay.lv2 ZamEQ2.lv2 ZamGate.lv2 ZamGateX2.lv2 ZamGEQ31.lv2 ZamHeadX2.lv2 ZamTube.lv2 ZaMultiComp.lv2 ZaMultiCompX2.lv2
 
-ZAM_PLUGINS_HOST_MAKE   = $(HOST_MAKE_ENV)   $(HOST_CONFIGURE_OPTS)   $(MAKE) NOOPT=true -C $(@D)
 ZAM_PLUGINS_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) NOOPT=true -C $(@D)
-
-ZAM_PLUGINS_TMP_DIR = $(HOST_DIR)/tmp-zam-plugins
 
 # needed for git submodules
 define ZAM_PLUGINS_EXTRACT_CMDS
@@ -24,46 +20,8 @@ define ZAM_PLUGINS_EXTRACT_CMDS
 	touch $(@D)/.stamp_downloaded
 endef
 
-define HOST_ZAM_PLUGINS_EXTRACT_CMDS
-	rm -rf $(@D)
-	git clone --recursive git://github.com/zamaudio/zam-plugins $(@D)
-	(cd $(@D) && \
-		git reset --hard $(ZAM_PLUGINS_VERSION) && \
-		git submodule update)
-	touch $(@D)/.stamp_downloaded
-endef
-
-# build plugins in host to generate ttls
-define HOST_ZAM_PLUGINS_BUILD_CMDS
-	# build everything
-	$(ZAM_PLUGINS_HOST_MAKE)
-
-	# delete binaries
-	rm $(@D)/bin/*.lv2/*.so
-
-	# create temp dir
-	rm -rf $(ZAM_PLUGINS_TMP_DIR)
-	mkdir -p $(ZAM_PLUGINS_TMP_DIR)
-
-	# copy the generated bundles without binaries to temp dir
-	cp -r $(@D)/bin/*.lv2 $(ZAM_PLUGINS_TMP_DIR)
-endef
-
-# build plugins in target skipping ttl generation
 define ZAM_PLUGINS_BUILD_CMDS
-	# create dummy generator
-	touch $(@D)/dpf/utils/lv2_ttl_generator
-	chmod +x $(@D)/dpf/utils/lv2_ttl_generator
-
-	# copy previously generated bundles
-	cp -r $(ZAM_PLUGINS_TMP_DIR)/*.lv2 $(@D)/bin/
-
-	# now build in target
 	$(ZAM_PLUGINS_TARGET_MAKE)
-
-	# cleanup
-	rm $(@D)/dpf/utils/lv2_ttl_generator
-	rm -r $(ZAM_PLUGINS_TMP_DIR)
 endef
 
 define ZAM_PLUGINS_INSTALL_TARGET_CMDS
@@ -84,4 +42,3 @@ define ZAM_PLUGINS_INSTALL_TARGET_CMDS
 endef
 
 $(eval $(generic-package))
-$(eval $(host-generic-package))
--- plugins/package/zynaddsubfx/zynaddsubfx.mk
+++ plugins/package/zynaddsubfx/zynaddsubfx.mk
@@ -22,27 +22,7 @@ define ZYNADDSUBFX_EXTRACT_CMDS
 	touch $(@D)/.stamp_downloaded
 endef
 
-# build plugins in target skipping ttl generation
 define ZYNADDSUBFX_BUILD_CMDS
-	# first build that fails
-	$(ZYNADDSUBFX_TARGET_MAKE) -k || true
-
-	# create dummy generator
-	rm $(@D)/src/Plugin/lv2-ttl-generator
-	touch $(@D)/src/Plugin/lv2-ttl-generator
-	chmod +x $(@D)/src/Plugin/lv2-ttl-generator
-
-	# copy previously generated ttl files
-	cp -rL $($(PKG)_PKGDIR)/ZynAddSubFX.lv2/*.ttl      $(@D)/src/Plugin/ZynAddSubFX/lv2/
-	cp -rL $($(PKG)_PKGDIR)/ZynAlienWah.lv2/*.ttl      $(@D)/src/Plugin/AlienWah/lv2/
-	cp -rL $($(PKG)_PKGDIR)/ZynChorus.lv2/*.ttl        $(@D)/src/Plugin/Chorus/lv2/
-	cp -rL $($(PKG)_PKGDIR)/ZynDistortion.lv2/*.ttl    $(@D)/src/Plugin/Distortion/lv2/
-	cp -rL $($(PKG)_PKGDIR)/ZynDynamicFilter.lv2/*.ttl $(@D)/src/Plugin/DynamicFilter/lv2/
-	cp -rL $($(PKG)_PKGDIR)/ZynEcho.lv2/*.ttl          $(@D)/src/Plugin/Echo/lv2/
-	cp -rL $($(PKG)_PKGDIR)/ZynPhaser.lv2/*.ttl        $(@D)/src/Plugin/Phaser/lv2/
-	cp -rL $($(PKG)_PKGDIR)/ZynReverb.lv2/*.ttl        $(@D)/src/Plugin/Reverb/lv2/
-
-	# final build
 	$(ZYNADDSUBFX_TARGET_MAKE)
 endef
 
