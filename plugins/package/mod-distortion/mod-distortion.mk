e0cc58f133df656d15b7be216b11d3d97c1a5ce8
jesseverhage <jesseverhage@gmail.com>
2016-08-19 16:21:45 +0200

    update mod-distortion

--- plugins/package/mod-distortion/mod-distortion.mk
+++ plugins/package/mod-distortion/mod-distortion.mk
@@ -4,9 +4,8 @@
 #
 ######################################
 
-MOD_DISTORTION_VERSION = 2df0209c516322a087637d43e4194b9922086b9dg
-MOD_DISTORTION_SITE_METHOD = local
-MOD_DISTORTION_SITE = /home/Jesse/mod-workdir/mod-distortion/
+MOD_DISTORTION_VERSION = 30fb491e43917b4653d8007597ca3e76afe9edd6
+MOD_DISTORTION_SITE = $(call github,moddevices,mod-distortion,$(MOD_DISTORTION_VERSION))
 MOD_DISTORTION_BUNDLES = mod-bigmuff.lv2 mod-ds1.lv2
 
 MOD_DISTORTION_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) NOOPT=true -C $(@D)
@@ -17,6 +16,8 @@ endef
 
 define MOD_DISTORTION_INSTALL_TARGET_CMDS
 	$(MOD_DISTORTION_TARGET_MAKE) install DESTDIR=$(TARGET_DIR) INSTALL_PATH=/usr/lib/lv2
+	cp -rL $($(PKG)_PKGDIR)/mod-bigmuff.lv2/* $(TARGET_DIR)/usr/lib/lv2/mod-bigmuff.lv2/
+	cp -rL $($(PKG)_PKGDIR)/mod-ds1.lv2/*     $(TARGET_DIR)/usr/lib/lv2/mod-ds1.lv2/
 endef
 
 $(eval $(generic-package))
