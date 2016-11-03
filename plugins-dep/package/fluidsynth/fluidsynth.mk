7c391826fc74e39c345bcb4a4bf0edc75b3edd7d
falkTX <falktx@gmail.com>
2016-11-03 15:34:31 +0100

    Add fluidsynth patch to build with -O3

--- plugins-dep/package/fluidsynth/fluidsynth.mk
+++ plugins-dep/package/fluidsynth/fluidsynth.mk
@@ -9,5 +9,6 @@ FLUIDSYNTH_SITE = http://download.sourceforge.net/fluidsynth
 FLUIDSYNTH_SOURCE = fluidsynth-$(FLUIDSYNTH_VERSION).tar.bz2
 FLUIDSYNTH_DEPENDENCIES = libglib2 libsndfile
 FLUIDSYNTH_INSTALL_STAGING = YES
+FLUIDSYNTH_AUTORECONF = YES
 
 $(eval $(autotools-package))
