90abaeb73d42970c51ad5635ab6ed7bd04126988
falkTX <falktx@gmail.com>
2016-11-03 11:45:43 +0100

    Fix pdlv2 package, buils and runs now

--- plugins/package/pdlv2/pdlv2.mk
+++ plugins/package/pdlv2/pdlv2.mk
@@ -4,19 +4,19 @@
 #
 ######################################
 
-PDLV2_VERSION = 408f111dfa1f50c6f3f0499f1c15a40264573edf
-PDLV2_SITE = $(call github,x37v,pdlv2,$(PDLV2_VERSION))
+PDLV2_VERSION = f8b8de956f649e481424b28cec9ca78174b21631
+PDLV2_SITE = $(call github,moddevices,pdlv2,$(PDLV2_VERSION))
 PDLV2_DEPENDENCIES = libpd lvtk
-# host-ruby
-PDLV2_BUNDLES = xxx.lv2
+PDLV2_BUNDLES = pdlv2-autopan.lv2 pdlv2-demo_distortion.lv2 pdlv2-demo_sine_synth.lv2 pdlv2-djfilter.lv2 pdlv2-midi_demo.lv2
 
 PDLV2_LVTK=$(STAGING_DIR)/usr/lib/liblvtk_plugin2.a
 PDLV2_LIBPD=$(STAGING_DIR)/usr/lib/libpd.so
 
-PDLV2_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) LVTKLIB=$(PDLV2_LVTK) LIBPD_SO=$(PDLV2_LIBPD) -C $(@D)
+PDLV2_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) LVTKLIB=$(PDLV2_LVTK) LIBPD_SO=$(PDLV2_LIBPD) RUBY=/usr/bin/ruby -C $(@D)
 
 define PDLV2_BUILD_CMDS
-# 	bundle install
+	(cd $(@D) && /usr/bin/bundle install)
+	(cd $(@D) && [ -d lvtk ] && [ ! -L lvtk ] && rmdir lvtk && ln -s $(STAGING_DIR)/usr/include/lvtk-2 lvtk || true)
 	$(PDLV2_TARGET_MAKE)
 endef
 
