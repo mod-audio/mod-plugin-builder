657961cdf3a97c3bf30352f371271434ea62c544
MrBollie <github@ca9.eu>
2016-11-14 20:19:25 +0100

    Initial check in of bolliedelay

---
+++ plugins/package/bolliedelay/bolliedelay.mk
@@ -0,0 +1,25 @@
+
+######################################
+#
+# bolliedelay
+#
+######################################
+
+BOLLIEDELAY_VERSION = f5d5dee5dd59b7431f5fe954ba2911d8f788ce8c
+BOLLIEDELAY_SITE = $(call github,MrBollie,bolliedelay.lv2,$(BOLLIEDELAY_VERSION))
+BOLLIEDELAY_DEPENDENCIES = 
+BOLLIEDELAY_BUNDLES = bolliedelay.lv2
+
+BOLLIEDELAY_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) MOD=1 OPTIMIZATIONS="" PREFIX=/usr -C $(@D)
+
+define BOLLIEDELAY_BUILD_CMDS
+	$(BOLLIEDELAY_TARGET_MAKE)
+endef
+
+define BOLLIEDELAY_INSTALL_TARGET_CMDS
+	$(BOLLIEDELAY_TARGET_MAKE) install DESTDIR=$(TARGET_DIR)
+#	cp -rL $($(PKG)_PKGDIR)/bolliedelay.lv2/* $(TARGET_DIR)/usr/lib/lv2/bolliedelay.lv2/
+endef
+
+$(eval $(generic-package))
+
