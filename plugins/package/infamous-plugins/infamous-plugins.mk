771f0685b5155335df95736452b3d9a2012b7710
ssj71 <ssjackson71@gmail.com>
2017-04-13 00:31:36 -0600

    adding more infamous plugins

--- plugins/package/infamous-plugins/infamous-plugins.mk
+++ plugins/package/infamous-plugins/infamous-plugins.mk
@@ -4,15 +4,18 @@
 #
 ######################################
 
-INFAMOUS_PLUGINS_VERSION = ca7ccc28934e5e3b8ab70d7ef66eb114396d2ed1
+INFAMOUS_PLUGINS_VERSION = 2c7e5e56cb643716342f1eb09ce4d7bc40316110
 INFAMOUS_PLUGINS_SITE = $(call github,ssj71,infamousPlugins,$(INFAMOUS_PLUGINS_VERSION))
 #envfollower.lv2 ewham.lv2 casynth.lv2 cheapdist.lv2 hip2b.lv2 lushlife.lv2 powercut.lv2 powerup.lv2 stuck.lv2 bentdelay.lv2
-INFAMOUS_PLUGINS_BUNDLES = ewham.lv2 stuck.lv2 powercut.lv2
+INFAMOUS_PLUGINS_BUNDLES = ewham.lv2 stuck.lv2 powercut.lv2 bentdelay.lv2 hip2b.lv2 mindi.lv2
 
 define INFAMOUS_PLUGINS_POST_INSTALL_TARGET_TTLFILES
 	cp -rL $($(PKG)_PKGDIR)/ewham.lv2/* $(TARGET_DIR)/usr/lib/lv2/ewham.lv2/
 	cp -rL $($(PKG)_PKGDIR)/stuck.lv2/* $(TARGET_DIR)/usr/lib/lv2/stuck.lv2/
-# 	cp -rL $($(PKG)_PKGDIR)/powercut.lv2/* $(TARGET_DIR)/usr/lib/lv2/powercut.lv2/
+ 	cp -rL $($(PKG)_PKGDIR)/powercut.lv2/* $(TARGET_DIR)/usr/lib/lv2/powercut.lv2/
+ 	cp -rL $($(PKG)_PKGDIR)/bentdelay.lv2/* $(TARGET_DIR)/usr/lib/lv2/bentdelay.lv2/
+ 	cp -rL $($(PKG)_PKGDIR)/hip2b.lv2/* $(TARGET_DIR)/usr/lib/lv2/hip2b.lv2/
+ 	cp -rL $($(PKG)_PKGDIR)/mindi.lv2/* $(TARGET_DIR)/usr/lib/lv2/mindi.lv2/
 endef
 
 INFAMOUS_PLUGINS_POST_INSTALL_TARGET_HOOKS += INFAMOUS_PLUGINS_POST_INSTALL_TARGET_TTLFILES
