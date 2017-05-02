41a0064bac8433f0b14e94e406c48a0e49dcf22d
falkTX <falktx@gmail.com>
2017-05-02 17:41:02 +0200

    Add new setbfree-mod package, for new b_whirl bundle

---
+++ plugins/package/setbfree-mod/setbfree-mod.mk
@@ -0,0 +1,26 @@
+######################################
+#
+# setbfree-mod
+#
+######################################
+
+SETBFREE_MOD_VERSION = 9c04792f1c16725eb978527add672eae2f938f26
+SETBFREE_MOD_SITE = $(call github,pantherb,setBfree,$(SETBFREE_MOD_VERSION))
+SETBFREE_MOD_DEPENDENCIES = fftwf libsndfile zita-convolver
+SETBFREE_MOD_BUNDLES = b_whirl_mod
+
+SETBFREE_MOD_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) MOD=1 OPTIMIZATIONS= PREFIX=/usr -C $(@D)
+
+define SETBFREE_MOD_BUILD_CMDS
+	$(SETBFREE_MOD_TARGET_MAKE)
+endef
+
+define SETBFREE_MOD_INSTALL_TARGET_CMDS
+	$(SETBFREE_MOD_TARGET_MAKE) install DESTDIR=$(TARGET_DIR)
+	
+	install -d $(TARGET_DIR)/usr/lib/lv2/b_whirl_mod
+	cp -rL $($(PKG)_PKGDIR)/b_whirl_mod/* $(TARGET_DIR)/usr/lib/lv2/b_whirl_mod/
+	cp $(TARGET_DIR)/usr/lib/lv2/b_whirl/*.so $(TARGET_DIR)/usr/lib/lv2/b_whirl_mod/
+endef
+
+$(eval $(generic-package))
