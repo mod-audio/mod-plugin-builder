aa47f3e29d2d7c5ae070c716db5cd5c1379c5912
falkTX <falktx@gmail.com>
2017-08-04 16:09:40 +0200

    Update distrho-ports

--- plugins/package/distrho-ports/distrho-ports.mk
+++ plugins/package/distrho-ports/distrho-ports.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-DISTRHO_PORTS_VERSION = 5fc083166e0837df9011c0212491b6bffcc1edfd
+DISTRHO_PORTS_VERSION = f591a1066cd3929536699bb516caa4b5efd9d025
 DISTRHO_PORTS_SITE = $(call github,DISTRHO,DISTRHO-Ports,$(DISTRHO_PORTS_VERSION))
 DISTRHO_PORTS_BUNDLES = drowaudio-distortion.lv2 drowaudio-distortionshaper.lv2 drowaudio-flanger.lv2 drowaudio-reverb.lv2 drowaudio-tremolo.lv2 Luftikus.lv2 Obxd.lv2 TAL-Dub-3.lv2 TAL-Filter-2.lv2 TAL-Filter.lv2 TAL-NoiseMaker.lv2 TAL-Reverb-2.lv2 TAL-Reverb-3.lv2 TAL-Reverb.lv2 TAL-Vocoder-2.lv2 TheFunction.lv2 ThePilgrim.lv2 Vex.lv2 Wolpertinger.lv2
 
@@ -15,13 +15,13 @@ define DISTRHO_PORTS_CONFIGURE_CMDS
 endef
 
 define DISTRHO_PORTS_BUILD_CMDS
-	$(DISTRHO_PORTS_TARGET_MAKE) lv2
+	$(DISTRHO_PORTS_TARGET_MAKE) lv2_nogen
 endef
 
 define DISTRHO_PORTS_INSTALL_TARGET_CMDS
 	install -d $(TARGET_DIR)/usr/lib/lv2/
-	cp -r $(@D)/bin/lv2/*.lv2/        $(TARGET_DIR)/usr/lib/lv2/
-# 	cp -r $(@D)/static-lv2-ttl/*.lv2/ $(TARGET_DIR)/usr/lib/lv2/
+	cp -r $(@D)/bin/lv2/*.lv2/    $(TARGET_DIR)/usr/lib/lv2/
+	cp -rL $($(PKG)_PKGDIR)/*.lv2 $(TARGET_DIR)/usr/lib/lv2/
 endef
 
 $(eval $(generic-package))
