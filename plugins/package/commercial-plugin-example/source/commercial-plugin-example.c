b4237b58aa812aa82ea92ecd9915e84a1416ae3a
Ricardo Crudo <ricardo.crudo@gmail.com>
2018-01-18 16:04:01 -0200

    add commercial plugin example

---
+++ plugins/package/commercial-plugin-example/source/commercial-plugin-example.c
@@ -0,0 +1,77 @@
+#include <libmodla.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+#define PLUGIN_URI "urn:mod:commercial-plugin-example"
+
+typedef struct {
+    const float* input;
+    float* output;
+    uint32_t cnt;
+} Plugin;
+
+static LV2_Handle plugin_instantiate(const LV2_Descriptor* d, double sr, const char* b, const LV2_Feature* const* f)
+{
+    mod_license_check(f, PLUGIN_URI);
+    return calloc(1, sizeof(Plugin));
+
+    // unused
+    (void)d;
+    (void)sr;
+    (void)b;
+}
+
+static void plugin_connect_port(LV2_Handle instance, uint32_t port, void* data_location)
+{
+    Plugin* const plugin = (Plugin*)instance;
+
+    switch (port)
+    {
+    case 0:
+        plugin->input = (const float*)data_location;
+        break;
+    case 1:
+        plugin->output = (float*)data_location;
+        break;
+    }
+}
+
+static void plugin_run(LV2_Handle instance, uint32_t sample_count)
+{
+    Plugin* const plugin = (Plugin*)instance;
+
+    plugin->cnt = mod_license_run_begin(plugin->cnt, sample_count);
+
+    if (plugin->input != plugin->output)
+        memcpy(plugin->output, plugin->input, sizeof(float)*sample_count);
+
+    mod_license_run_noise(plugin->cnt, plugin->output, sample_count, 0);
+}
+
+static void plugin_cleanup(LV2_Handle instance)
+{
+    free(instance);
+}
+
+static const void* plugin_extension_data(const char* uri)
+{
+    return mod_license_interface(uri);
+}
+
+static const LV2_Descriptor plugin_descriptor = {
+    PLUGIN_URI,
+    plugin_instantiate,
+    plugin_connect_port,
+    NULL,
+    plugin_run,
+    NULL,
+    plugin_cleanup,
+    plugin_extension_data
+};
+
+LV2_SYMBOL_EXPORT
+const LV2_Descriptor* lv2_descriptor(uint32_t index)
+{
+    return (index == 0) ? &plugin_descriptor : NULL;
+}
