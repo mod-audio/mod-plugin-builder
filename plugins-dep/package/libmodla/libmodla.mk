042909d6afb1452ef6324047408348b99cf3500d
Ricardo Crudo <ricardo.crudo@gmail.com>
2018-01-18 16:02:55 -0200

    add libmodla

---
+++ plugins-dep/package/libmodla/libmodla.mk
@@ -0,0 +1,17 @@
+######################################
+#
+# libmodla
+#
+######################################
+
+LIBMODLA_VERSION = v1.0.0
+LIBMODLA_SITE = http://web1.moddevices.com/shared/
+LIBMODLA_SOURCE = libmodla-$(LIBMODLA_VERSION).tar.gz
+LIBMODLA_INSTALL_STAGING = YES
+
+define LIBMODLA_INSTALL_STAGING_CMDS
+	(cd $(@D); cp mod-license.h libmodla.h $(STAGING_DIR)/usr/include)
+	(cd $(@D); cp libmodla.a $(STAGING_DIR)/usr/lib)
+endef
+
+$(eval $(generic-package))
