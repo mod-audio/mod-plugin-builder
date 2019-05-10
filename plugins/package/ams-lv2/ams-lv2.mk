2d58841604acca46c9a3d621473bbf7faa037a2d
falkTX <falktx@falktx.com>
2019-05-10 23:48:30 +0200

    Fix ams-lv2 packages, regarding fftw[-double]
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/ams-lv2/ams-lv2.mk
+++ plugins/package/ams-lv2/ams-lv2.mk
@@ -6,7 +6,7 @@
 
 AMS_LV2_VERSION = 0f60ee0a0e6df64877210dfee2d30f126dc3137f
 AMS_LV2_SITE = $(call github,blablack,ams-lv2,$(AMS_LV2_VERSION))
-AMS_LV2_DEPENDENCIES = fftw lvtk-1
+AMS_LV2_DEPENDENCIES = fftw-double lvtk-1
 AMS_LV2_BUNDLES = ams.lv2
 
 AMS_LV2_TARGET_WAF = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(HOST_DIR)/usr/bin/python ./waf
