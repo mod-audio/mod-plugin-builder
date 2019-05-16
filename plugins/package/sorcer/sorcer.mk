29705865e38af76ed20f093adf69434ef9047a75
falkTX <falktx@falktx.com>
2019-05-16 21:37:14 +0200

    Working sorcer plugin package
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/sorcer/sorcer.mk
+++ plugins/package/sorcer/sorcer.mk
@@ -4,8 +4,8 @@
 #
 ######################################
 
-SORCER_VERSION = 9da6481cb609cd3aa7a9c988fab2f66571c2b04b
+SORCER_VERSION = 0a8cef484174aae5c1b7be6710f31a643e7d7197
 SORCER_SITE = $(call github,harryhaaren,openAV-Sorcer,$(SORCER_VERSION))
-SORCER_BUNDLES = 
+SORCER_BUNDLES = sorcer.lv2
 
 $(eval $(cmake-package))
