73ef21c3a9cf46e15bcae1adc1c6c2a483fd8818
falkTX <falktx@falktx.com>
2019-05-16 22:40:27 +0200

    Working synthv1 plugin package
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/synthv1/synthv1.mk
+++ plugins/package/synthv1/synthv1.mk
@@ -4,10 +4,16 @@
 #
 ######################################
 
-SYNTHV1_VERSION = 0.7.1
+SYNTHV1_VERSION = 0.9.7
 SYNTHV1_SITE = http://download.sourceforge.net/synthv1/
 SYNTHV1_SOURCE = synthv1-$(SYNTHV1_VERSION).tar.gz
-SYNTHV1_DEPENDENCIES = qt5base host-qt5base
-SYNTHV1_BUNDLES = 
+SYNTHV1_BUNDLES = synthv1.lv2
+SYNTHV1_AUTORECONF = YES
+
+SYNTHV1_CONF_OPTS=--disable-jack --disable-lv2-ui-x11 --disable-lv2-ui-external
+
+define SYNTHV1_CONFIGURE_TARGET_CMDS_EXTRA
+	rm -f $(@D)/src/synthv1.lv2/synthv1_ui.ttl
+endef
 
 $(eval $(autotools-package))
