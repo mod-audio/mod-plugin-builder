38079dc6990016eefa6d7aea40d3c2e809fee46e
falkTX <falktx@falktx.com>
2019-05-28 15:54:12 +0200

    Fix broken zam-plugins build
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/zam-plugins/zam-plugins.mk
+++ plugins/package/zam-plugins/zam-plugins.mk
@@ -25,6 +25,9 @@ define ZAM_PLUGINS_BUILD_CMDS
 endef
 
 define ZAM_PLUGINS_INSTALL_TARGET_CMDS
+	# fixes broken install
+	touch $(@D)/bin/ZamPhono
+
 	$(ZAM_PLUGINS_TARGET_MAKE) install PREFIX=/usr DESTDIR=$(TARGET_DIR)
 	# TODO ZamDynamicEQ.lv2 ZamPhono.lv2
 	cp -rL $($(PKG)_PKGDIR)/ZamAutoSat.lv2/*    $(TARGET_DIR)/usr/lib/lv2/ZamAutoSat.lv2/
