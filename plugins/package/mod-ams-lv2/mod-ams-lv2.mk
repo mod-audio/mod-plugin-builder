9185a1ad4b2076811fa828040b66c3a87dc792da
falkTX <falktx@falktx.com>
2019-05-30 22:05:03 +0200

    Update mod-ams-lv2
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/mod-ams-lv2/mod-ams-lv2.mk
+++ plugins/package/mod-ams-lv2/mod-ams-lv2.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-MOD_AMS_LV2_VERSION = 65d3a0a3362aec43a4eed77036fe1fd19fd3656d
+MOD_AMS_LV2_VERSION = f20bffbc4005658979403ea444b2ede94ac64c55
 MOD_AMS_LV2_SITE = $(call github,moddevices,ams-lv2,$(MOD_AMS_LV2_VERSION))
 MOD_AMS_LV2_DEPENDENCIES = fftw-double lvtk-1
 MOD_AMS_LV2_BUNDLES = mod-ams.lv2
