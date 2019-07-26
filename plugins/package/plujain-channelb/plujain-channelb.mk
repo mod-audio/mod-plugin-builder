0fc33bdf0e0019f4d54d49da67f44b31431f608e
houston <picotmathieu@gmail.com>
2019-07-26 17:17:47 +0200

    add .mk file for Plujain channelB plugin

---
+++ plugins/package/plujain-channelb/plujain-channelb.mk
@@ -0,0 +1,39 @@
+######################################
+#
+# channelb-lv2
+#
+######################################
+
+# where to find the source code
+PLUJAIN_CHANNELB_VERSION = 167b7cf301459e6adbff7e3b82b34353e3bf7f18
+PLUJAIN_CHANNELB_SITE = $(call github,Houston4444,plujain-channelb,$(PLUJAIN_CHANNELB_VERSION))
+# PLUJAIN_CHANNELB_SITE_METHOD = local
+# PLUJAIN_CHANNELB_SITE = $($(PKG)_PKGDIR)/
+
+# even though this is a local build, we still need a version number
+# bump this number if you need to force a rebuild
+# PLUJAIN_CHANNELB_VERSION = 1
+
+# dependencies (list of other buildroot packages, separated by space)
+PLUJAIN_CHANNELB_DEPENDENCIES =
+
+# LV2 bundles that this package generates (space separated list)
+PLUJAIN_CHANNELB_BUNDLES = plujain-channelb.lv2
+
+# call make with the current arguments and path. "$(@D)" is the build directory.
+PLUJAIN_CHANNELB_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
+
+
+# build command
+define PLUJAIN_CHANNELB_BUILD_CMDS
+	$(PLUJAIN_CHANNELB_TARGET_MAKE)
+endef
+
+# install command
+define PLUJAIN_CHANNELB_INSTALL_TARGET_CMDS
+	$(PLUJAIN_CHANNELB_TARGET_MAKE) install DESTDIR=$(TARGET_DIR)
+endef
+
+
+# import everything else from the buildroot generic package
+$(eval $(generic-package))
