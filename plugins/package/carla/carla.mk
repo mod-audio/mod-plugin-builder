487e461d5d2b16770c82d54a0b98e178cac9417e
falkTX <falktx@gmail.com>
2019-08-18 14:24:00 +0100

    Add carla package

---
+++ plugins/package/carla/carla.mk
@@ -0,0 +1,27 @@
+######################################
+#
+# abgate
+#
+######################################
+
+CARLA_VERSION = 5ecbedbcc1a25b15d9fc03fba5c5ed448028840e
+CARLA_SITE = $(call github,falkTX,Carla,$(CARLA_VERSION))
+CARLA_DEPENDENCIES = libsndfile
+# FIXME ffmpeg fluidsynth as dependencies, plus liblo eventually
+CARLA_BUNDLES = carla.lv2
+
+CARLA_NO_DEPS = HAVE_FFMPEG=false HAVE_FLUIDSYNTH=false HAVE_LIBLO=false
+CARLA_NO_GUI  = HAVE_HYLIA=false HAVE_PYQT=false
+
+CARLA_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(CARLA_NO_DEPS) $(CARLA_NO_GUI) NOOPT=true -C $(@D)
+
+define CARLA_BUILD_CMDS
+	$(CARLA_TARGET_MAKE)
+endef
+
+define CARLA_INSTALL_TARGET_CMDS
+	$(CARLA_TARGET_MAKE) install DESTDIR=$(TARGET_DIR)
+	# cp -rL $($(PKG)_PKGDIR)/carla.lv2/* $(TARGET_DIR)/usr/lib/lv2/carla.lv2/
+endef
+
+$(eval $(generic-package))
