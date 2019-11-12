dc5a4a107fb47109f06de0f3152ad3dc4590a0c5
falkTX <falktx@falktx.com>
2019-11-12 13:00:25 +0100

    Update synthv1

--- plugins/package/synthv1/synthv1.mk
+++ plugins/package/synthv1/synthv1.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-SYNTHV1_VERSION = 0.9.7
+SYNTHV1_VERSION = 0.9.11
 SYNTHV1_SITE = http://download.sourceforge.net/synthv1/
 SYNTHV1_SOURCE = synthv1-$(SYNTHV1_VERSION).tar.gz
 SYNTHV1_BUNDLES = synthv1.lv2
@@ -12,8 +12,8 @@ SYNTHV1_AUTORECONF = YES
 
 SYNTHV1_CONF_OPTS=--disable-jack --disable-lv2-ui-x11 --disable-lv2-ui-external
 
-define SYNTHV1_CONFIGURE_TARGET_CMDS_EXTRA
-	rm -f $(@D)/src/synthv1.lv2/synthv1_ui.ttl
-endef
+# define SYNTHV1_CONFIGURE_TARGET_CMDS_EXTRA
+# 	rm -f $(@D)/src/synthv1.lv2/synthv1_ui.ttl
+# endef
 
 $(eval $(autotools-package))
