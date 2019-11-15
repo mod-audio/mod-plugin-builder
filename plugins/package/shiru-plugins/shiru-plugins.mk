a96c84a2910ebb2fde36cccc8949cf82bdd09fab
falkTX <falktx@falktx.com>
2019-11-15 14:56:27 +0000

    Fix shiru-plugins build
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/shiru-plugins/shiru-plugins.mk
+++ plugins/package/shiru-plugins/shiru-plugins.mk
@@ -8,7 +8,7 @@ SHIRU_PLUGINS_VERSION = 089f4b1bbe7e19ada24d826cdfa86b6079d3430b
 SHIRU_PLUGINS_SITE = $(call github,linuxmao-org,shiru-plugins,$(SHIRU_PLUGINS_VERSION))
 SHIRU_PLUGINS_BUNDLES = 1bs_bitdrive.lv2 1bs_clickydrums.lv2 1bs_noisedrums.lv2 crushdmc.lv2 flopster.lv2
 
-SHIRU_PLUGINS_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) NOOPT=true -C $(@D)
+SHIRU_PLUGINS_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) NOOPT=true HAVE_CAIRO=false HAVE_DGL=false -C $(@D)
 
 # needed for git submodules
 define SHIRU_PLUGINS_EXTRACT_CMDS
