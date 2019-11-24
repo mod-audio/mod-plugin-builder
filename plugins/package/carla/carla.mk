6370c1e81cb937d357e708b82f08f197231ece18
falkTX <falktx@falktx.com>
2019-11-24 16:02:28 +0000

    Update carla package, only build the lv2 plugins
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/carla/carla.mk
+++ plugins/package/carla/carla.mk
@@ -4,24 +4,24 @@
 #
 ######################################
 
-CARLA_VERSION = 8385b5ee418357e2dc907518ac39c20beb3b28ed
+CARLA_VERSION = 8e8fe2e111b669fcd38be1d146cdf9e612949fa9
 CARLA_SITE = $(call github,falkTX,Carla,$(CARLA_VERSION))
-CARLA_DEPENDENCIES = libsndfile
-# FIXME ffmpeg fluidsynth as dependencies, plus liblo eventually
-CARLA_BUNDLES = carla.lv2
+CARLA_DEPENDENCIES = libsndfile ffmpeg
+# for later: fluidsynth liblo (unused at the moment)
+CARLA_BUNDLES = carla-audiogain.lv2 carla-files.lv2 carla-miditools.lv2
 
-CARLA_NO_DEPS = HAVE_FFMPEG=false HAVE_FLUIDSYNTH=false HAVE_LIBLO=false
-CARLA_NO_GUI  = HAVE_HYLIA=false HAVE_PYQT=false
+CARLA_NO_GUI = HAVE_HYLIA=false HAVE_PYQT=false
 
-CARLA_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(CARLA_NO_DEPS) $(CARLA_NO_GUI) NOOPT=true -C $(@D)
+CARLA_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(CARLA_NO_GUI) NOOPT=true -C $(@D) lv2-bundles
 
 define CARLA_BUILD_CMDS
 	$(CARLA_TARGET_MAKE)
 endef
 
 define CARLA_INSTALL_TARGET_CMDS
-	$(CARLA_TARGET_MAKE) install DESTDIR=$(TARGET_DIR)
-	# cp -rL $($(PKG)_PKGDIR)/carla.lv2/* $(TARGET_DIR)/usr/lib/lv2/carla.lv2/
+	install -d $(TARGET_DIR)/usr/lib/lv2/
+	cp -rL $(@D)/bin/carla-*.lv2 $(TARGET_DIR)/usr/lib/lv2/
+	cp -rL $($(PKG)_PKGDIR)/carla-audiogain.lv2/* $(TARGET_DIR)/usr/lib/lv2/carla-audiogain.lv2/
 endef
 
 $(eval $(generic-package))
