a72be808023bebeac5d55cdacb054ba7401cdb5c
BramGiesen <giesenbram@gmail.com>
2019-12-19 13:48:41 +0100

    Added new lv2-data

---
+++ lv2-data-creative-commons/README.md
@@ -0,0 +1,4 @@
+# LV2 data - Creative Commons
+
+This repository contains the lv2-data from plugins that use custom made GUIs by MOD Devices. All GUIs in this repository are licensed under a Creative Commons by-nc-nd 4.0 license.
+For more information about this license see: <https://creativecommons.org/licenses/by-nc-nd/4.0>
--- plugins/package/infamous-plugins/infamous-plugins.mk
+++ plugins/package/infamous-plugins/infamous-plugins.mk
@@ -12,6 +12,7 @@ INFAMOUS_PLUGINS_BUNDLES = ewham.lv2 stuck.lv2 powercut.lv2 bentdelay.lv2 hip2b.
 define INFAMOUS_PLUGINS_POST_INSTALL_TARGET_TTLFILES
 	cp -rL $($(PKG)_PKGDIR)/ewham.lv2/* $(TARGET_DIR)/usr/lib/lv2/ewham.lv2/
 	cp -rL $($(PKG)_PKGDIR)/stuck.lv2/* $(TARGET_DIR)/usr/lib/lv2/stuck.lv2/
+	cp -rL $($(PKG)_PKGDIR)/mindi.lv2/* $(TARGET_DIR)/usr/lib/lv2/mindi.lv2/
 endef
 
 INFAMOUS_PLUGINS_POST_INSTALL_TARGET_HOOKS += INFAMOUS_PLUGINS_POST_INSTALL_TARGET_TTLFILES
