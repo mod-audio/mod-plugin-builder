774c87dda4f18c71372256a76bc2d017cd17aca6
BramGiesen <bramgiesen@thinkpad-t460.localdomain>
2020-01-03 13:15:13 +0100

    Updated commit hash for setbfree plugin

--- plugins/package/setbfree/setbfree.mk
+++ plugins/package/setbfree/setbfree.mk
@@ -4,12 +4,12 @@
 #
 ######################################
 
-SETBFREE_VERSION = 30f1ec6267afb0ee17b472db0c4826d5bd96fad8
+SETBFREE_VERSION = 93e7f154bee67590d6d321a572a1b107f8fc36e1
 SETBFREE_SITE = $(call github,pantherb,setBfree,$(SETBFREE_VERSION))
 SETBFREE_DEPENDENCIES = fftw-single libsndfile zita-convolver
 SETBFREE_BUNDLES = b_overdrive b_reverb b_synth b_whirl b_whirl_xt
 
-SETBFREE_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) MOD_BUILD=1 OPTIMIZATIONS= PREFIX=/usr -C $(@D)
+SETBFREE_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) INSTALL_EXTRA_LV2=yes MOD_BUILD=1 OPTIMIZATIONS= PREFIX=/usr -C $(@D)
 
 define SETBFREE_BUILD_CMDS
 	$(SETBFREE_TARGET_MAKE)
@@ -20,7 +20,7 @@ define SETBFREE_INSTALL_TARGET_CMDS
 	cp -rL $($(PKG)_PKGDIR)/b_overdrive/* $(TARGET_DIR)/usr/lib/lv2/b_overdrive/
 	cp -rL $($(PKG)_PKGDIR)/b_reverb/*    $(TARGET_DIR)/usr/lib/lv2/b_reverb/
 	cp -rL $($(PKG)_PKGDIR)/b_synth/*     $(TARGET_DIR)/usr/lib/lv2/b_synth/
-	
+
 	# b_whirl has been manually splitted into separate bundles
 	mv $(TARGET_DIR)/usr/lib/lv2/b_whirl/b_whirl.so $(TARGET_DIR)/usr/lib/lv2/b_whirl.so
 	rm -rf $(TARGET_DIR)/usr/lib/lv2/b_whirl/
