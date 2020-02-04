f44548ada07dd89427f6b91a4fd3d2a5c5c771bf
BramGiesen <giesenbram@gmail.com>
2020-02-04 19:28:47 +0100

    Update commit hash for mod-cv-plugins and lv2-data

--- plugins/package/mod-cv-plugins/mod-cv-plugins.mk
+++ plugins/package/mod-cv-plugins/mod-cv-plugins.mk
@@ -4,9 +4,9 @@
 #
 ######################################
 
-MOD_CV_PLUGINS_VERSION = 8dee8b707ddc8e9f515f7c6c92fc3271080824ef
+MOD_CV_PLUGINS_VERSION = c001fe4143cdd288dc597cff02960405ba337fa3
 MOD_CV_PLUGINS_SITE = $(call github,moddevices,mod-cv-plugins,$(MOD_CV_PLUGINS_VERSION))
-MOD_CV_PLUGINS_BUNDLES = mod-audio-to-cv.lv2 mod-cv-attenuverter.lv2 mod-cv-clock.lv2 mod-cv-control.lv2 mod-cv-switch1.lv2 mod-cv-switch2.lv2 mod-cv-switch3.lv2 mod-cv-switch4.lv2 mod-midi-to-cv-mono.lv2 mod-midi-to-cv-poly.lv2 mod-cv-meter.lv2 mod-cv-random.lv2 mod-cv-slew.lv2 mod-cv-gate.lv2 mod-cv-range.lv2 mod-cv-round.lv2 mod-cv-abs.lv2
+MOD_CV_PLUGINS_BUNDLES = mod-audio-to-cv.lv2 mod-cv-attenuverter.lv2 mod-cv-clock.lv2 mod-cv-control.lv2 mod-cv-switch1.lv2 mod-cv-switch2.lv2 mod-cv-switch3.lv2 mod-cv-switch4.lv2 mod-midi-to-cv-mono.lv2 mod-midi-to-cv-poly.lv2 mod-cv-meter.lv2 mod-cv-random.lv2 mod-cv-slew.lv2 mod-cv-gate.lv2 mod-cv-range.lv2 mod-cv-round.lv2 mod-cv-abs.lv2 mod-cv-parameter-modulation.lv2
 
 # dependencies (list of other buildroot packages, separated by space)
 MOD_CV_PLUGINS_DEPENDENCIES =
@@ -20,9 +20,9 @@ endef
 
 define MOD_CV_PLUGINS_INSTALL_TARGET_CMDS
 	install -d $(TARGET_DIR)/usr/lib/lv2/
-	cp -r $(@D)/mod-cv*/source/*.lv2    $(TARGET_DIR)/usr/lib/lv2/
-	cp -r $(@D)/mod-midi*/source/*.lv2  $(TARGET_DIR)/usr/lib/lv2/
-	cp -r $(@D)/mod-audio*/source/*.lv2 $(TARGET_DIR)/usr/lib/lv2/
+	cp -r $(@D)/source/mod-cv*/*.lv2    $(TARGET_DIR)/usr/lib/lv2/
+	cp -r $(@D)/source/mod-midi*/*.lv2  $(TARGET_DIR)/usr/lib/lv2/
+	cp -r $(@D)/source/mod-audio*/*.lv2 $(TARGET_DIR)/usr/lib/lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-audio-to-cv.lv2/*        $(TARGET_DIR)/usr/lib/lv2/mod-audio-to-cv.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-cv-attenuverter.lv2/*    $(TARGET_DIR)/usr/lib/lv2/mod-cv-attenuverter.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-cv-clock.lv2/*           $(TARGET_DIR)/usr/lib/lv2/mod-cv-clock.lv2/
@@ -37,6 +37,7 @@ define MOD_CV_PLUGINS_INSTALL_TARGET_CMDS
 	cp -rL $($(PKG)_PKGDIR)/mod-cv-switch4.lv2/*         $(TARGET_DIR)/usr/lib/lv2/mod-cv-switch4.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-midi-to-cv-mono.lv2/*    $(TARGET_DIR)/usr/lib/lv2/mod-midi-to-cv-mono.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-midi-to-cv-poly.lv2/*    $(TARGET_DIR)/usr/lib/lv2/mod-midi-to-cv-poly.lv2/
+	cp -rL $($(PKG)_PKGDIR)/mod-cv-parameter-modulation.lv2/* $(TARGET_DIR)/usr/lib/lv2/mod-cv-parameter-modulation.lv2/
 endef
 
 $(eval $(generic-package))
