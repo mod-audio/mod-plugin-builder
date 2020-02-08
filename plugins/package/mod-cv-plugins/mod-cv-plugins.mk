54e0212d155f5991c6d5519820dfa247ca52fd09
BramGiesen <giesenbram@gmail.com>
2020-02-08 15:50:01 +0100

    Update commit mod-cv-plugins
    Updated commit hash mod-cv-plugins package and lv2-data-creative-commons

--- plugins/package/mod-cv-plugins/mod-cv-plugins.mk
+++ plugins/package/mod-cv-plugins/mod-cv-plugins.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-MOD_CV_PLUGINS_VERSION = c001fe4143cdd288dc597cff02960405ba337fa3
+MOD_CV_PLUGINS_VERSION = e04a882435a3253fab2a913fb29fb46dedcd795a
 MOD_CV_PLUGINS_SITE = $(call github,moddevices,mod-cv-plugins,$(MOD_CV_PLUGINS_VERSION))
 MOD_CV_PLUGINS_BUNDLES = mod-audio-to-cv.lv2 mod-cv-attenuverter.lv2 mod-cv-clock.lv2 mod-cv-control.lv2 mod-cv-switch1.lv2 mod-cv-switch2.lv2 mod-cv-switch3.lv2 mod-cv-switch4.lv2 mod-midi-to-cv-mono.lv2 mod-midi-to-cv-poly.lv2 mod-cv-meter.lv2 mod-cv-random.lv2 mod-cv-slew.lv2 mod-cv-gate.lv2 mod-cv-range.lv2 mod-cv-round.lv2 mod-cv-abs.lv2 mod-cv-parameter-modulation.lv2
 
