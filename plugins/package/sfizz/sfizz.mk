39691d511082884060df0f75c1449dc35be86f3e
falkTX <falktx@falktx.com>
2020-03-11 09:56:42 +0000

    Update sfizz to working PR and add patch for filetypes

--- plugins/package/sfizz/sfizz.mk
+++ plugins/package/sfizz/sfizz.mk
@@ -4,19 +4,16 @@
 #
 ######################################
 
-SFIZZ_VERSION = 65ca77c70d8eb35ee5cdd87103c7d1ca32906c29
-SFIZZ_SITE = $(call github,sfztools,sfizz,$(SFIZZ_VERSION))
+SFIZZ_VERSION = 82e4a7404404f1c78abf30beec5b8e95abc81c49
+SFIZZ_SITE = $(call github,paulfd,sfizz,$(SFIZZ_VERSION))
 SFIZZ_DEPENDENCIES = libsndfile
 SFIZZ_CONF_OPTS=-DSFIZZ_JACK=OFF
 SFIZZ_BUNDLES = sfizz.lv2
 
-SFIZZ_CMAKE = /usr/bin/cmake
-# /home/falktx/mod-workdir/modduox/host/usr/bin/cmake
-
 # needed for git submodules
 define SFIZZ_EXTRACT_CMDS
 	rm -rf $(@D)
-	git clone --recursive git://github.com/sfztools/sfizz $(@D)
+	git clone --recursive git://github.com/paulfd/sfizz $(@D)
 	(cd $(@D) && \
 		git reset --hard $(SFIZZ_VERSION) && \
 		git submodule update)
