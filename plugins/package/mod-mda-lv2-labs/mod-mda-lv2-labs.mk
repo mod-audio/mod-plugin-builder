01184ea14636ce675aa32d7ff1ad9fb1d868291e
BramGiesen <giesenbram@gmail.com>
2020-07-23 12:53:06 +0200

    Renamed .mk file to match package name

--- plugins/package/mod-mda-lv2-labs/mod-mda-lv2-labs.mk
+++ plugins/package/mod-mda-lv2-labs/mod-mda-lv2-labs.mk
@@ -1,25 +1,25 @@
 ######################################
 #
-# mod-mda-lv2
+# mod-mda-lv2-labs
 #
 ######################################
 
-MOD_MDA_LV2_VERSION = 3d6dd099146b72c1fe88e06679034715fb999a5b
-MOD_MDA_LV2_SITE = $(call github,moddevices,mda-lv2,$(MOD_MDA_LV2_VERSION))
-MOD_MDA_LV2_BUNDLES = mod-mda-Bandisto.lv2 mod-mda-Combo.lv2 mod-mda-DeEss.lv2 mod-mda-Degrade.lv2 mod-mda-Delay.lv2 mod-mda-Dither.lv2 mod-mda-Dynamics.lv2 mod-mda-Image.lv2 mod-mda-Limiter.lv2 mod-mda-Loudness.lv2 mod-mda-MultiBand.lv2 mod-mda-RezFilter.lv2 mod-mda-Shepard.lv2 mod-mda-Splitter.lv2 mod-mda-Stereo.lv2 mod-mda-TalkBox.lv2 mod-mda-TestTone.lv2 mod-mda-Tracker.lv2 mod-mda-Transient.lv2 mod-mda-VocInput.lv2
+MOD_MDA_LV2_LABS_VERSION = 3d6dd099146b72c1fe88e06679034715fb999a5b
+MOD_MDA_LV2_LABS_SITE = $(call github,moddevices,mda-lv2,$(MOD_MDA_LV2_LABS_VERSION))
+MOD_MDA_LV2_LABS_BUNDLES = mod-mda-Bandisto.lv2 mod-mda-Combo.lv2 mod-mda-DeEss.lv2 mod-mda-Degrade.lv2 mod-mda-Delay.lv2 mod-mda-Dither.lv2 mod-mda-Dynamics.lv2 mod-mda-Image.lv2 mod-mda-Limiter.lv2 mod-mda-Loudness.lv2 mod-mda-MultiBand.lv2 mod-mda-RezFilter.lv2 mod-mda-Shepard.lv2 mod-mda-Splitter.lv2 mod-mda-Stereo.lv2 mod-mda-TalkBox.lv2 mod-mda-TestTone.lv2 mod-mda-Tracker.lv2 mod-mda-Transient.lv2 mod-mda-VocInput.lv2
 
-MOD_MDA_LV2_TARGET_WAF = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(HOST_DIR)/usr/bin/python2 ./waf
+MOD_MDA_LV2_LABS_TARGET_WAF = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(HOST_DIR)/usr/bin/python2 ./waf
 
-define MOD_MDA_LV2_CONFIGURE_CMDS
-	(cd $(@D); $(MOD_MDA_LV2_TARGET_WAF) configure --prefix=/usr)
+define MOD_MDA_LV2_LABS_CONFIGURE_CMDS
+	(cd $(@D); $(MOD_MDA_LV2_LABS_TARGET_WAF) configure --prefix=/usr)
 endef
 
-define MOD_MDA_LV2_BUILD_CMDS
-	(cd $(@D); $(MOD_MDA_LV2_TARGET_WAF) build -j $(PARALLEL_JOBS))
+define MOD_MDA_LV2_LABS_BUILD_CMDS
+	(cd $(@D); $(MOD_MDA_LV2_LABS_TARGET_WAF) build -j $(PARALLEL_JOBS))
 endef
 
-define MOD_MDA_LV2_INSTALL_TARGET_CMDS
-	(cd $(@D); $(MOD_MDA_LV2_TARGET_WAF) install --destdir=$(TARGET_DIR) -j 1)
+define MOD_MDA_LV2_LABS_INSTALL_TARGET_CMDS
+	(cd $(@D); $(MOD_MDA_LV2_LABS_TARGET_WAF) install --destdir=$(TARGET_DIR) -j 1)
 	cp -rL $($(PKG)_PKGDIR)/mod-mda-Bandisto.lv2/*  $(TARGET_DIR)/usr/lib/lv2/mod-mda-Bandisto.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-mda-Combo.lv2/*     $(TARGET_DIR)/usr/lib/lv2/mod-mda-Combo.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-mda-DeEss.lv2/*     $(TARGET_DIR)/usr/lib/lv2/mod-mda-DeEss.lv2/
