88846d82a2f4b9399b3a78e4a1890a978d0b312e
BramGiesen <giesenbram@gmail.com>
2020-08-11 13:05:37 +0200

    Renamed eg-amp-lv2 to eg-amp-lv2-labs

--- plugins/package/eg-amp-lv2-labs/eg-amp-lv2-labs.mk
+++ plugins/package/eg-amp-lv2-labs/eg-amp-lv2-labs.mk
@@ -5,31 +5,31 @@
 ######################################
 
 # where to find the source code - locally in this case
-EG_AMP_LV2_SITE_METHOD = local
-EG_AMP_LV2_SITE = $($(PKG)_PKGDIR)/
+EG_AMP_LV2_LABS_SITE_METHOD = local
+EG_AMP_LV2_LABS_SITE = $($(PKG)_PKGDIR)/
 
 # even though this is a local build, we still need a version number
 # bump this number if you need to force a rebuild
-EG_AMP_LV2_VERSION = 1
+EG_AMP_LV2_LABS_VERSION = 1
 
 # dependencies (list of other buildroot packages, separated by space)
-EG_AMP_LV2_DEPENDENCIES =
+EG_AMP_LV2_LABS_DEPENDENCIES =
 
 # LV2 bundles that this package generates (space separated list)
-EG_AMP_LV2_BUNDLES = eg-amp.lv2
+EG_AMP_LV2_LABS_BUNDLES = eg-amp.lv2
 
 # call make with the current arguments and path. "$(@D)" is the build directory.
-EG_AMP_LV2_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/source
+EG_AMP_LV2_LABS_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/source
 
 
 # build command
-define EG_AMP_LV2_BUILD_CMDS
-	$(EG_AMP_LV2_TARGET_MAKE)
+define EG_AMP_LV2_LABS_BUILD_CMDS
+	$(EG_AMP_LV2_LABS_TARGET_MAKE)
 endef
 
 # install command
-define EG_AMP_LV2_INSTALL_TARGET_CMDS
-	$(EG_AMP_LV2_TARGET_MAKE) install DESTDIR=$(TARGET_DIR)
+define EG_AMP_LV2_LABS_INSTALL_TARGET_CMDS
+	$(EG_AMP_LV2_LABS_TARGET_MAKE) install DESTDIR=$(TARGET_DIR)
 endef
 
 
