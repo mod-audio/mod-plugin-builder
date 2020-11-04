489da735ae815747d7bb5bdc26a345475b52b085
BramGiesen <giesenbram@gmail.com>
2020-11-04 16:34:18 +0100

    Added new switchboxes to .mk file

--- plugins/package/mod-utilities/mod-utilities.mk
+++ plugins/package/mod-utilities/mod-utilities.mk
@@ -4,10 +4,10 @@
 #
 ######################################
 
-MOD_UTILITIES_VERSION = 4a92aa81351c5c95fefa0505fafad5fbea494dd0
+MOD_UTILITIES_VERSION = 0bdff8811c20faee93a798c3d7d8bc2b9945f761
 MOD_UTILITIES_SITE = $(call github,moddevices,mod-utilities,$(MOD_UTILITIES_VERSION))
 MOD_UTILITIES_DEPENDENCIES = alsa-lib
-MOD_UTILITIES_BUNDLES = mod-bpf.lv2 mod-bypass.lv2 mod-crossover2.lv2 mod-crossover3.lv2 mod-gain.lv2 mod-gain2x2.lv2 mod-hpf.lv2 mod-lpf.lv2 mod-switchbox2.lv2 mod-toggleswitch4.lv2
+MOD_UTILITIES_BUNDLES = mod-bpf.lv2 mod-bypass.lv2 mod-crossover2.lv2 mod-crossover3.lv2 mod-gain.lv2 mod-gain2x2.lv2 mod-hpf.lv2 mod-lpf.lv2 mod-switchbox2.lv2 mod-toggleswitch4.lv2 switchbox_1-2_st.lv2 switchbox_2-1.lv2 switchbox_2-1_st.lv2
 
 MOD_UTILITIES_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) NOOPT=true MOD=1 -C $(@D)
 
@@ -17,6 +17,7 @@ endef
 
 define MOD_UTILITIES_INSTALL_TARGET_CMDS
 	$(MOD_UTILITIES_TARGET_MAKE) install DESTDIR=$(TARGET_DIR) INSTALL_PATH=/usr/lib/lv2
+	mv $(TARGET_DIR)/usr/lib/lv2/switchbox_1-2.lv2   $(TARGET_DIR)/usr/lib/lv2/mod-switchbox2.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-bpf.lv2/*            $(TARGET_DIR)/usr/lib/lv2/mod-bpf.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-bypass.lv2/*         $(TARGET_DIR)/usr/lib/lv2/mod-bypass.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-crossover2.lv2/*     $(TARGET_DIR)/usr/lib/lv2/mod-crossover2.lv2/
@@ -27,6 +28,9 @@ define MOD_UTILITIES_INSTALL_TARGET_CMDS
 	cp -rL $($(PKG)_PKGDIR)/mod-lpf.lv2/*            $(TARGET_DIR)/usr/lib/lv2/mod-lpf.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-switchbox2.lv2/*     $(TARGET_DIR)/usr/lib/lv2/mod-switchbox2.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-toggleswitch4.lv2/*  $(TARGET_DIR)/usr/lib/lv2/mod-toggleswitch4.lv2/
+	cp -rL $($(PKG)_PKGDIR)/switchbox_1-2_st.lv2/*   $(TARGET_DIR)/usr/lib/lv2/switchbox_1-2_st.lv2/
+	cp -rL $($(PKG)_PKGDIR)/switchbox_2-1.lv2/*      $(TARGET_DIR)/usr/lib/lv2/switchbox_2-1.lv2/
+	cp -rL $($(PKG)_PKGDIR)/switchbox_2-1_st.lv2/*   $(TARGET_DIR)/usr/lib/lv2/switchbox_2-1_st.lv2/
 endef
 
 $(eval $(generic-package))
