5bb34100b0153972c4c7d0b488b3e540a9669c65
falkTX <falktx@falktx.com>
2020-11-29 23:15:14 +0000

    Add modgui to eg-sampler, add eg-params ttl for easier testing

--- plugins/package/lv2-examples-labs/lv2-examples-labs.mk
+++ plugins/package/lv2-examples-labs/lv2-examples-labs.mk
@@ -4,13 +4,23 @@
 #
 ######################################
 
-LV2_EXAMPLES_LABS_VERSION = 1.18.0
-LV2_EXAMPLES_LABS_SITE = http://lv2plug.in/spec
-LV2_EXAMPLES_LABS_SOURCE = lv2-$(LV2_EXAMPLES_LABS_VERSION).tar.bz2
+LV2_EXAMPLES_LABS_VERSION = bd93d6d7008ed7d6eabd8f5df3d37e6f969cb183
+LV2_EXAMPLES_LABS_SITE = https://github.com/lv2/lv2.git
+LV2_EXAMPLES_LABS_SITE_METHOD = git
 LV2_EXAMPLES_LABS_DEPENDENCIES = host-python
-LV2_EXAMPLES_LABS_BUNDLES = eg-amp.lv2 eg-fifths.lv2 eg-midigate.lv2 eg-sampler.lv2
+LV2_EXAMPLES_LABS_BUNDLES = eg-amp.lv2 eg-fifths.lv2 eg-midigate.lv2 eg-params.lv2 eg-sampler.lv2
 
 LV2_EXAMPLES_LABS_TARGET_WAF = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(HOST_DIR)/usr/bin/python ./waf
+
+# needed for git submodules
+define LV2_EXAMPLES_LABS_EXTRACT_CMDS
+	rm -rf $(@D)
+	git clone --recursive $(LV2_EXAMPLES_LABS_SITE) $(@D)
+	(cd $(@D) && \
+		git reset --hard $(LV2_EXAMPLES_LABS_VERSION) && \
+		git submodule update)
+	touch $(@D)/.stamp_downloaded
+endef
 
 define LV2_EXAMPLES_LABS_CONFIGURE_CMDS
 	(cd $(@D); $(LV2_EXAMPLES_LABS_TARGET_WAF) configure --prefix=/usr)
@@ -23,6 +33,7 @@ endef
 define LV2_EXAMPLES_LABS_INSTALL_TARGET_CMDS
 	(cd $(@D); $(LV2_EXAMPLES_LABS_TARGET_WAF) install --destdir=$(TARGET_DIR))
 	cp -rL $($(PKG)_PKGDIR)/eg-sampler.lv2/* $(TARGET_DIR)/usr/lib/lv2/eg-sampler.lv2/
+	cp -rL $($(PKG)_PKGDIR)/eg-params.lv2/*  $(TARGET_DIR)/usr/lib/lv2/eg-params.lv2/
 endef
 
 $(eval $(generic-package))
