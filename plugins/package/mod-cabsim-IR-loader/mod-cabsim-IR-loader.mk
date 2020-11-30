e92f84edc69b1c31282bb8fdf21b10ffa3ddc2c5
BramGiesen <giesenbram@gmail.com>
2020-11-30 21:24:41 +0100

    Add package for cabsim IR loader

---
+++ plugins/package/mod-cabsim-IR-loader/mod-cabsim-IR-loader.mk
@@ -0,0 +1,22 @@
+######################################
+#
+# cabsim-IR-loader
+#
+######################################
+
+MOD_CABSIM_IR_LOADER_VERSION = e9e1a1ea1e22e5053229b998b83a261716fb407b
+MOD_CABSIM_IR_LOADER_SITE = $(call github,moddevices,mod-cabsim-IR-loader,$(MOD_CABSIM_IR_LOADER_VERSION))
+
+MOD_CABSIM_IR_LOADER_BUNDLES = cabsim-IR-loader.lv2
+
+MOD_CABSIM_IR_LOADER_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/source
+
+define MOD_CABSIM_IR_LOADER_BUILD_CMDS
+	$(MOD_CABSIM_IR_LOADER_TARGET_MAKE)
+endef
+
+define MOD_CABSIM_IR_LOADER_INSTALL_TARGET_CMDS
+	$(MOD_CABSIM_IR_LOADER_TARGET_MAKE) install DESTDIR=$(TARGET_DIR)
+endef
+
+$(eval $(generic-package))
