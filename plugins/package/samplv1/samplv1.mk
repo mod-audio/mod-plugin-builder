0f43a895df7cf9163cfc54bcfcb14d747a58e75a
BramGiesen <giesenbram@gmail.com>
2020-12-03 17:17:20 +0100

    Updated samplv1 package

--- plugins/package/samplv1/samplv1.mk
+++ plugins/package/samplv1/samplv1.mk
@@ -12,4 +12,8 @@ SAMPLV1_AUTORECONF = YES
 
 SAMPLV1_CONF_OPTS=--disable-jack --disable-lv2-ui-x11 --disable-lv2-ui-external
 
+define SAMPLV1_INSTALL_TARGET_CMDS
+	cp -rL $($(PKG)_PKGDIR)/samplv1.lv2/* $(TARGET_DIR)/usr/lib/lv2/samplv1.lv2/
+endef
+
 $(eval $(autotools-package))
