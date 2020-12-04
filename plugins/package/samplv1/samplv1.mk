7d5f7d6d9236ec621ec79ab80b7df0e6d7c200c3
BramGiesen <giesenbram@gmail.com>
2020-12-04 15:58:09 +0100

    Extended patch to remove some qt checks + mk

--- plugins/package/samplv1/samplv1.mk
+++ plugins/package/samplv1/samplv1.mk
@@ -12,8 +12,10 @@ SAMPLV1_AUTORECONF = YES
 
 SAMPLV1_CONF_OPTS=--disable-jack --disable-lv2-ui-x11 --disable-lv2-ui-external
 
-define SAMPLV1_INSTALL_TARGET_CMDS
-	cp -rL $($(PKG)_PKGDIR)/samplv1.lv2/* $(TARGET_DIR)/usr/lib/lv2/samplv1.lv2/
+define SAMPLV1_COPY_LV2_DATA
+	cp -rL $($(PKG)_PKGDIR)/$(SAMPLV1_BUNDLES)/* $(TARGET_DIR)/usr/lib/lv2/$(SAMPLV1_BUNDLES)
 endef
+
+SAMPLV1_POST_INSTALL_TARGET_HOOKS += SAMPLV1_COPY_LV2_DATA
 
 $(eval $(autotools-package))
