5f57c6ee73df78af6866cce634cc149ad6a2e175
BramGiesen <giesenbram@gmail.com>
2021-01-25 13:06:51 +0100

    Update samplv1 package

--- plugins/package/samplv1/samplv1.mk
+++ plugins/package/samplv1/samplv1.mk
@@ -4,14 +4,14 @@
 #
 ######################################
 
-SAMPLV1_VERSION = 0037f2c1a2845aaacc938eaff0bc680314fdf434
+SAMPLV1_VERSION = 85c688e833b43adcfde1045ed4933c72ab85c987
 SAMPLV1_SITE = $(call github,moddevices,samplv1,$(SAMPLV1_VERSION))
 SAMPLV1_BUNDLES = samplv1.lv2
 SAMPLV1_AUTORECONF = YES
 
 SAMPLV1_CONF_OPTS=--disable-jack --disable-lv2-ui-x11 --disable-lv2-ui-external --enable-lv2-port-event=no
 
-define MOD_TAL_NOISEMAKER_CONFIGURE_CMDS
+define SAMPLV1_CONFIGURE_CMDS
        (cd $(@D); ./autogen.sh)
 endef
 
