5371ae880657cc6bcf366ea6ce5b35f86831e4a6
BramGiesen <giesenbram@gmail.com>
2021-01-25 15:44:35 +0100

    Add audio mixers

---
+++ plugins/package/mod-audio-mixers/mod-audio-mixers.mk
@@ -0,0 +1,33 @@
+######################################
+#
+# mod-audio-mixers
+#
+######################################
+
+MOD_AUDIO_MIXERS_VERSION = 0e967da29933bb771ad8bbab67fb6a4f498077f0
+MOD_AUDIO_MIXERS_SITE = $(call github,moddevices,mod-audio-mixer-lv2,$(MOD_AUDIO_MIXERS_VERSION))
+MOD_AUDIO_MIXERS_BUNDLES = mod-mixer.lv2 mod-mixer-stereo.lv2
+
+MOD_AUDIO_MIXERS_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
+
+# needed for git submodules
+define MOD_AUDIO_MIXERS_EXTRACT_CMDS
+	rm -rf $(@D)
+	git clone --recursive git://github.com/moddevices/mod-audio-mixer-lv2 $(@D)
+	(cd $(@D) && \
+		git reset --hard $(MOD_AUDIO_MIXERS_VERSION) && \
+		git submodule update)
+	touch $(@D)/.stamp_downloaded
+endef
+
+define MOD_AUDIO_MIXERS_BUILD_CMDS
+	$(MOD_AUDIO_MIXERS_TARGET_MAKE)
+endef
+
+define MOD_AUDIO_MIXERS_INSTALL_TARGET_CMDS
+	$(MOD_AUDIO_MIXERS_TARGET_MAKE) install PREFIX=/usr DESTDIR=$(TARGET_DIR)
+	cp -rL $($(PKG)_PKGDIR)/mod-mixer.lv2/*       $(TARGET_DIR)/usr/lib/lv2/mod-mixer.lv2/
+	cp -rL $($(PKG)_PKGDIR)/mod-mixer-stereo.lv2/*       $(TARGET_DIR)/usr/lib/lv2/mod-mixer-stereo.lv2/
+endef
+
+$(eval $(generic-package))
