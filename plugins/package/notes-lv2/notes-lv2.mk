741c400efe3b3fa031c85f5ad40a3736cc455ba7
falkTX <falktx@falktx.com>
2021-02-12 15:50:43 +0000

    Set notes-lv2 depends on lv2, temporary test for build check
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/notes-lv2/notes-lv2.mk
+++ plugins/package/notes-lv2/notes-lv2.mk
@@ -7,6 +7,7 @@
 NOTES_LV2_VERSION = 0.2.0
 NOTES_LV2_SITE = https://git.open-music-kontrollers.ch/lv2/notes.lv2/snapshot
 NOTES_LV2_SOURCE = notes.lv2-$(NOTES_LV2_VERSION).tar.xz
+NOTES_LV2_DEPENDENCIES = lv2
 NOTES_LV2_BUNDLES = notes.lv2
 
 NOTES_LV2_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
