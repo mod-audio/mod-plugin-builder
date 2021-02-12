52699b946aa0410cbb4c521e3ac40b26f9095abf
falkTX <falktx@falktx.com>
2021-02-12 03:21:18 +0000

    Update to fix carla-files against bad mod-host; Disable ffmpeg
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/carla-plugins-labs/carla-plugins-labs.mk
+++ plugins/package/carla-plugins-labs/carla-plugins-labs.mk
@@ -4,13 +4,12 @@
 #
 ######################################
 
-CARLA_PLUGINS_LABS_VERSION = 7537be13ae2230cca9cdc9aa3d78313db10464a5
+CARLA_PLUGINS_LABS_VERSION = 066ae08df391cf923eb0f0022e5182b1a8712a14
 CARLA_PLUGINS_LABS_SITE = $(call github,falkTX,Carla,$(CARLA_PLUGINS_LABS_VERSION))
-CARLA_PLUGINS_LABS_DEPENDENCIES = libsndfile ffmpeg
-# for later: fluidsynth liblo (unused at the moment)
+CARLA_PLUGINS_LABS_DEPENDENCIES = libsndfile
 CARLA_PLUGINS_LABS_BUNDLES = carla-audiogain.lv2 carla-files.lv2 carla-miditools.lv2
 
-CARLA_PLUGINS_LABS_NO_GUI = HAVE_HYLIA=false HAVE_PYQT=false
+CARLA_PLUGINS_LABS_NO_GUI = HAVE_FFMPEG=false HAVE_HYLIA=false HAVE_PYQT=false
 
 CARLA_PLUGINS_LABS_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(CARLA_PLUGINS_LABS_NO_GUI) NOOPT=true -C $(@D) lv2-bundles
 
