44aa9419e515b9fe4dba723e84fd855e0199b345
BramGiesen <giesenbram@gmail.com>
2021-02-22 17:47:15 +0100

    Move eg-sampler to lv2-examples

--- plugins/package/lv2-examples-labs/lv2-examples-labs.mk
+++ plugins/package/lv2-examples-labs/lv2-examples-labs.mk
@@ -8,7 +8,7 @@ LV2_EXAMPLES_LABS_VERSION = 64d4a4a46901b1b1ab4eaac4f3f57f8d353e42c5
 LV2_EXAMPLES_LABS_SITE = https://github.com/lv2/lv2.git
 LV2_EXAMPLES_LABS_SITE_METHOD = git
 LV2_EXAMPLES_LABS_DEPENDENCIES = host-python
-LV2_EXAMPLES_LABS_BUNDLES = eg-amp.lv2 eg-fifths.lv2 eg-midigate.lv2 eg-params.lv2 eg-sampler.lv2
+LV2_EXAMPLES_LABS_BUNDLES = eg-amp.lv2 eg-fifths.lv2 eg-midigate.lv2 eg-params.lv2
 
 LV2_EXAMPLES_LABS_TARGET_WAF = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(HOST_DIR)/usr/bin/python ./waf
 
@@ -32,7 +32,6 @@ endef
 
 define LV2_EXAMPLES_LABS_INSTALL_TARGET_CMDS
 	(cd $(@D); $(LV2_EXAMPLES_LABS_TARGET_WAF) install --destdir=$(TARGET_DIR))
-	cp -rL $($(PKG)_PKGDIR)/eg-sampler.lv2/* $(TARGET_DIR)/usr/lib/lv2/eg-sampler.lv2/
 	cp -rL $($(PKG)_PKGDIR)/eg-params.lv2/*  $(TARGET_DIR)/usr/lib/lv2/eg-params.lv2/
 endef
 
--- plugins/package/lv2-examples/lv2-examples.mk
+++ plugins/package/lv2-examples/lv2-examples.mk
@@ -4,13 +4,23 @@
 #
 ######################################
 
-LV2_EXAMPLES_VERSION = 1.18.0
-LV2_EXAMPLES_SITE = http://lv2plug.in/spec
-LV2_EXAMPLES_SOURCE = lv2-$(LV2_EXAMPLES_VERSION).tar.bz2
+LV2_EXAMPLES_VERSION = 64d4a4a46901b1b1ab4eaac4f3f57f8d353e42c5
+LV2_EXAMPLES_SITE = https://github.com/lv2/lv2.git
+LV2_EXAMPLES_SITE_METHOD = git
 LV2_EXAMPLES_DEPENDENCIES = host-python
-LV2_EXAMPLES_BUNDLES = eg-metro.lv2
+LV2_EXAMPLES_BUNDLES = eg-metro.lv2 eg-sampler.lv2
 
 LV2_EXAMPLES_TARGET_WAF = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(HOST_DIR)/usr/bin/python ./waf
+
+# needed for git submodules
+define LV2_EXAMPLES_EXTRACT_CMDS
+	rm -rf $(@D)
+	git clone --recursive $(LV2_EXAMPLES_SITE) $(@D)
+	(cd $(@D) && \
+		git reset --hard $(LV2_EXAMPLES_VERSION) && \
+		git submodule update)
+	touch $(@D)/.stamp_downloaded
+endef
 
 define LV2_EXAMPLES_CONFIGURE_CMDS
 	(cd $(@D); $(LV2_EXAMPLES_TARGET_WAF) configure --prefix=/usr)
@@ -22,6 +32,7 @@ endef
 
 define LV2_EXAMPLES_INSTALL_TARGET_CMDS
 	(cd $(@D); $(LV2_EXAMPLES_TARGET_WAF) install --destdir=$(TARGET_DIR))
+	cp -rL $($(PKG)_PKGDIR)/eg-sampler.lv2/* $(TARGET_DIR)/usr/lib/lv2/eg-sampler.lv2/
 	cp -rL $($(PKG)_PKGDIR)/eg-metro.lv2/* $(TARGET_DIR)/usr/lib/lv2/eg-metro.lv2/
 endef
 
