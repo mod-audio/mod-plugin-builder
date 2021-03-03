20a6dc926a1ab678ba19f2c19a8ad34c4db6378f
falkTX <falktx@falktx.com>
2021-03-03 12:57:22 +0000

    Fix tal-reverb install
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/tal-reverb/tal-reverb.mk
+++ plugins/package/tal-reverb/tal-reverb.mk
@@ -6,7 +6,7 @@
 
 TAL_REVERB_VERSION = 58485ff6b7559f3a470e9924ab0074bae163ea63
 TAL_REVERB_SITE = $(call github,DISTRHO,DISTRHO-Ports,$(TAL_REVERB_VERSION))
-TAL_REVERB_BUNDLES = TAL-Reverb-2.lv2
+TAL_REVERB_BUNDLES = mod-tal-Reverb-2.lv2
 
 TAL_REVERB_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) LINUX_EMBED=true -C $(@D)
 
@@ -20,8 +20,9 @@ endef
 
 define TAL_REVERB_INSTALL_TARGET_CMDS
 	install -d $(TARGET_DIR)/usr/lib/lv2/
-	cp -r $(@D)/bin/lv2/TAL-Reverb-2.lv2/        $(TARGET_DIR)/usr/lib/lv2/mod-tal-Reverb-2.lv2
-	cp -rL $($(PKG)_PKGDIR)/mod-tal-Reverb-2.lv2 $(TARGET_DIR)/usr/lib/lv2/
+	rm -rf $(TARGET_DIR)/usr/lib/lv2/mod-tal-Reverb-2.lv2
+	cp -r $(@D)/bin/lv2/TAL-Reverb-2.lv2           $(TARGET_DIR)/usr/lib/lv2/mod-tal-Reverb-2.lv2
+	cp -rL $($(PKG)_PKGDIR)/mod-tal-Reverb-2.lv2/* $(TARGET_DIR)/usr/lib/lv2/mod-tal-Reverb-2.lv2/
 endef
 
 $(eval $(generic-package))
