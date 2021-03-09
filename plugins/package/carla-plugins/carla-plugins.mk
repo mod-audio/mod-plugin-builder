6f1df2b28e10c5c4bbcb36f80a09219adb82f7f2
falkTX <falktx@falktx.com>
2021-03-09 11:12:14 +0000

    Split carla-plugins from carla-plugins-labs
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/carla-plugins/carla-plugins.mk
+++ plugins/package/carla-plugins/carla-plugins.mk
@@ -1,13 +1,13 @@
 ######################################
 #
-# carla-plugins-labs
+# carla-plugins
 #
 ######################################
 
 CARLA_PLUGINS_LABS_VERSION = 57c570a31fcfe427d75a679a6f70f5fec640991b
 CARLA_PLUGINS_LABS_SITE = $(call github,falkTX,Carla,$(CARLA_PLUGINS_LABS_VERSION))
 CARLA_PLUGINS_LABS_DEPENDENCIES = libsndfile
-CARLA_PLUGINS_LABS_BUNDLES = carla-audiogain.lv2 carla-miditools.lv2
+CARLA_PLUGINS_LABS_BUNDLES = carla-files.lv2
 
 CARLA_PLUGINS_LABS_NO_GUI = HAVE_FFMPEG=false HAVE_HYLIA=false HAVE_PYQT=false
 
@@ -20,7 +20,7 @@ endef
 define CARLA_PLUGINS_LABS_INSTALL_TARGET_CMDS
 	install -d $(TARGET_DIR)/usr/lib/lv2/
 	cp -rL $(@D)/bin/carla-*.lv2 $(TARGET_DIR)/usr/lib/lv2/
-	cp -rL $($(PKG)_PKGDIR)/carla-audiogain.lv2/* $(TARGET_DIR)/usr/lib/lv2/carla-audiogain.lv2/
+	cp -rL $($(PKG)_PKGDIR)/carla-files.lv2/* $(TARGET_DIR)/usr/lib/lv2/carla-files.lv2/
 endef
 
 $(eval $(generic-package))
