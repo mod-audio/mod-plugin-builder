309b5c1e1dafa08ddf6dc729e8a94a009fb4f309
falkTX <falktx@falktx.com>
2021-03-09 12:26:59 +0000

    Update libsndfile for opus support and better seeking
    Signed-off-by: falkTX <falktx@falktx.com>

--- global-packages/libsndfile/libsndfile.mk
+++ global-packages/libsndfile/libsndfile.mk
@@ -4,11 +4,17 @@
 #
 ################################################################################
 
-LIBSNDFILE_VERSION = 1.0.27
-LIBSNDFILE_SITE = http://www.mega-nerd.com/libsndfile/files
+LIBSNDFILE_VERSION = 1.0.31
+LIBSNDFILE_SITE = https://github.com/libsndfile/libsndfile/releases/download/$(LIBSNDFILE_VERSION)
+LIBSNDFILE_SOURCE = libsndfile-$(LIBSNDFILE_VERSION).tar.bz2
 LIBSNDFILE_INSTALL_STAGING = YES
 LIBSNDFILE_LICENSE = LGPLv2.1+
 LIBSNDFILE_LICENSE_FILES = COPYING
-LIBSNDFILE_DEPENDENCIES = flac libogg libvorbis
+LIBSNDFILE_DEPENDENCIES = flac libogg libvorbis opus-custom
+LIBSNDFILE_AUTORECONF = YES
+
+LIBSNDFILE_CONF_OPTS = \
+	--disable-sqlite \
+	--disable-alsa
 
 $(eval $(autotools-package))
