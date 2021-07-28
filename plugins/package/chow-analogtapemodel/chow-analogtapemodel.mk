020d4d0a4a4ab3d84f1576779ff7eb4ff0b560e0
falkTX <falktx@falktx.com>
2021-07-28 19:05:13 +0100

    Fixup chow-analogtapemodel, now builds
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/chow-analogtapemodel/chow-analogtapemodel.mk
+++ plugins/package/chow-analogtapemodel/chow-analogtapemodel.mk
@@ -8,7 +8,7 @@ CHOW_ANALOGTAPEMODEL_VERSION = b3d284be1f0e391c72eb22d05953f8047a7a03eb
 CHOW_ANALOGTAPEMODEL_SITE = $(call github,jatinchowdhury18,AnalogTapeModel,$(CHOW_ANALOGTAPEMODEL_VERSION))
 # TODO make some libs optional
 CHOW_ANALOGTAPEMODEL_DEPENDENCIES = freetype jack2mod xlib_libXcursor xlib_libXinerama xlib_libXrandr host-cmake
-CHOW_ANALOGTAPEMODEL_BUNDLES = ChowAnalogTapeModel.lv2
+CHOW_ANALOGTAPEMODEL_BUNDLES = CHOWTapeModel.lv2
 
 # call make with the current arguments and path. "$(@D)" is the build directory.
 CHOW_ANALOGTAPEMODEL_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/Plugin
@@ -30,17 +30,18 @@ define CHOW_ANALOGTAPEMODEL_CONFIGURE_CMDS
 			-DCMAKE_C_COMPILER=$(TARGET_CC) \
 			-DCMAKE_CXX_COMPILER=$(TARGET_CXX) \
 			-DCMAKE_INSTALL_PREFIX="/usr" \
-			-DCMAKE_BUILD_TYPE=Release)
+			-DCMAKE_BUILD_TYPE=Release \
+			-DRTNEURAL_EIGEN=ON)
 endef
 
 define CHOW_ANALOGTAPEMODEL_BUILD_CMDS
-	$(CHOW_ANALOGTAPEMODEL_TARGET_MAKE)
+	$(CHOW_ANALOGTAPEMODEL_TARGET_MAKE) -k
 endef
 
 define CHOW_ANALOGTAPEMODEL_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/lib/lv2/ChowAnalogTapeModel.lv2
-	cp -rL $($(PKG)_PKGDIR)/ChowAnalogTapeModel.lv2/* $(TARGET_DIR)/usr/lib/lv2/ChowAnalogTapeModel.lv2/
-	cp -rL $(@D)/ChowCentaur/ChowCentaur_artefacts/Release/LV2/ChowAnalogTapeModel.lv2/* $(TARGET_DIR)/usr/lib/lv2/ChowAnalogTapeModel.lv2/
+	mkdir -p $(TARGET_DIR)/usr/lib/lv2/CHOWTapeModel.lv2
+	cp -rL $($(PKG)_PKGDIR)/CHOWTapeModel.lv2/* $(TARGET_DIR)/usr/lib/lv2/CHOWTapeModel.lv2/
+	cp -rL $(@D)/Plugin/CHOWTapeModel_artefacts/Release/LV2/CHOWTapeModel.lv2/* $(TARGET_DIR)/usr/lib/lv2/CHOWTapeModel.lv2/
 endef
 
 $(eval $(generic-package))
