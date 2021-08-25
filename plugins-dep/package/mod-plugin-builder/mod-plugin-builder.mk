0590895e0e569ed39927eef24c65b34cec002efb
falkTX <falktx@falktx.com>
2021-08-25 23:48:51 +0100

    Add initial juce; MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES

--- plugins-dep/package/mod-plugin-builder/mod-plugin-builder.mk
+++ plugins-dep/package/mod-plugin-builder/mod-plugin-builder.mk
@@ -13,4 +13,15 @@ MOD_PLUGIN_BUILDER_DEPENDENCIES = host-cmake
 define MOD_PLUGIN_BUILDER_EXTRACT_CMDS
 endef
 
+define MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES
+	if [ ! -e $($(PKG)_DL_DIR).tar.gz ]; then                         \
+		rm -rf $(@D);                                                 \
+		git clone --recursive $($(PKG)_SITE) $(@D);                   \
+		git -C $(@D) reset --hard $($(PKG)_VERSION);                  \
+		git -C $(@D) submodule update;                                \
+		tar --exclude=".git" -czf $($(PKG)_DL_DIR).tar.gz -C $(@D) .; \
+		touch $(@D)/.stamp_downloaded $(@D)/.stamp_extracted;         \
+	fi
+endef
+
 $(eval $(generic-package))
