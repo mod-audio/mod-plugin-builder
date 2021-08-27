d469669466d5771456412bd8c33a246f2c036b83
falkTX <falktx@falktx.com>
2021-08-27 05:00:43 +0100

    Same treatment for chow-centaur
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/chow-centaur/chow-centaur.mk
+++ plugins/package/chow-centaur/chow-centaur.mk
@@ -10,7 +10,7 @@ CHOW_CENTAUR_SITE_METHOD = git
 CHOW_CENTAUR_DEPENDENCIES = juce host-juce
 # TODO make some jack2mod optional
 CHOW_CENTAUR_DEPENDENCIES += jack2mod
-CHOW_CENTAUR_CONF_ENV = MOD_TOOLCHAIN_FILE=/home/builder/mod-plugin-builder/toolchainfile.cmake
+CHOW_CENTAUR_CONF_ENV = MOD_TOOLCHAIN_FILE=$(BR2_EXTERNAL)/package/juce/toolchain.cmake
 CHOW_CENTAUR_BUNDLES = ChowCentaur.lv2
 
 # needed for submodules support
