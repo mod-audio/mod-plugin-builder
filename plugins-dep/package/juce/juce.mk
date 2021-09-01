f0c3c3e69f6ceb018828e431f48118a99ddad243
falkTX <falktx@falktx.com>
2021-09-01 18:26:13 +0100

    Update chow tapemodel ttl, add xinerama and xrandr deps for now
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins-dep/package/juce/juce.mk
+++ plugins-dep/package/juce/juce.mk
@@ -6,7 +6,13 @@
 
 JUCE_VERSION = d10bc63cd1d9959e15f4713b15ebe0199e1ca996
 JUCE_SITE = $(call github,DISTRHO,juce,$(JUCE_VERSION))
-JUCE_DEPENDENCIES = freetype xlib_libXcursor
+JUCE_DEPENDENCIES = freetype xlib_libXcursor host-cmake
+HOST_JUCE_DEPENDENCIES = host-cmake host-freetype host-xlib_libXcursor
+
+# FIXME build with those disabled
+JUCE_DEPENDENCIES += xlib_libXinerama xlib_libXrandr
+
+# TODO actually build it
 
 $(eval $(generic-package))
 $(eval $(host-generic-package))
