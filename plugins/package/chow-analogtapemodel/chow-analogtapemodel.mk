c6e7e019c4dd96b0b315bcc432d8cd7adf007f38
falkTX <falktx@falktx.com>
2021-09-01 18:07:30 +0100

    Update chow-analogtapemodel stuff, refresh patches
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/chow-analogtapemodel/chow-analogtapemodel.mk
+++ plugins/package/chow-analogtapemodel/chow-analogtapemodel.mk
@@ -5,38 +5,16 @@
 ######################################
 
 CHOW_ANALOGTAPEMODEL_VERSION = 139ab8b5c2030d432ec258887f852a274582a862
-CHOW_ANALOGTAPEMODEL_SITE = $(call github,jatinchowdhury18,AnalogTapeModel,$(CHOW_ANALOGTAPEMODEL_VERSION))
-# TODO make some libs optional
-CHOW_ANALOGTAPEMODEL_DEPENDENCIES = freetype xlib_libXcursor xlib_libXinerama xlib_libXrandr host-cmake
+CHOW_ANALOGTAPEMODEL_SITE = https://github.com/jatinchowdhury18/AnalogTapeModel.git
+CHOW_ANALOGTAPEMODEL_SITE_METHOD = git
+CHOW_ANALOGTAPEMODEL_DEPENDENCIES = juce host-juce
+CHOW_ANALOGTAPEMODEL_CONF_ENV = MOD_TOOLCHAIN_FILE=$(BR2_EXTERNAL)/package/juce/toolchain.cmake
+# CHOW_ANALOGTAPEMODEL_CONF_OPTS = -DRTNEURAL_EIGEN=ON
+CHOW_ANALOGTAPEMODEL_SUBDIR = Plugin
 CHOW_ANALOGTAPEMODEL_BUNDLES = CHOWTapeModel.lv2
 
-# call make with the current arguments and path. "$(@D)" is the build directory.
-CHOW_ANALOGTAPEMODEL_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/Plugin
-
-# needed for git submodules
-define CHOW_ANALOGTAPEMODEL_EXTRACT_CMDS
-	rm -rf $(@D)
-	git clone --recursive git://github.com/jatinchowdhury18/AnalogTapeModel $(@D)
-	(cd $(@D) && \
-		git reset --hard $(CHOW_ANALOGTAPEMODEL_VERSION) && \
-		git submodule update)
-	touch $(@D)/.stamp_downloaded
-endef
-
-define CHOW_ANALOGTAPEMODEL_CONFIGURE_CMDS
-	(cd $(@D)/Plugin && \
-		rm -f CMakeCache.txt && \
-		"$(HOST_DIR)/usr/bin/cmake" . \
-			-DCMAKE_C_COMPILER=$(TARGET_CC) \
-			-DCMAKE_CXX_COMPILER=$(TARGET_CXX) \
-			-DCMAKE_INSTALL_PREFIX="/usr" \
-			-DCMAKE_BUILD_TYPE=Release \
-			-DRTNEURAL_EIGEN=ON)
-endef
-
-define CHOW_ANALOGTAPEMODEL_BUILD_CMDS
-	$(CHOW_ANALOGTAPEMODEL_TARGET_MAKE) -k
-endef
+# needed for submodules support
+CHOW_ANALOGTAPEMODEL_PRE_DOWNLOAD_HOOKS += MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES
 
 define CHOW_ANALOGTAPEMODEL_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib/lv2/CHOWTapeModel.lv2
@@ -44,4 +22,4 @@ define CHOW_ANALOGTAPEMODEL_INSTALL_TARGET_CMDS
 	cp -rL $(@D)/Plugin/CHOWTapeModel_artefacts/Release/LV2/CHOWTapeModel.lv2/* $(TARGET_DIR)/usr/lib/lv2/CHOWTapeModel.lv2/
 endef
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
