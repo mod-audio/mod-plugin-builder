70d14c4480509b5de6f01ad32df9d5cb70a8c008
falkTX <falktx@falktx.com>
2021-09-11 20:00:56 +0100

    Update artyfx
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/artyfx/artyfx.mk
+++ plugins/package/artyfx/artyfx.mk
@@ -4,16 +4,24 @@
 #
 ######################################
 
-ARTYFX_VERSION = 3fdeecd081c9a03b3a630d2ed0825674a30c05d5
+ARTYFX_VERSION = 9b147f25c5b0e45e52c10d9b148ff04e7cc24951
 ARTYFX_SITE = $(call github,harryhaaren,openAV-ArtyFX,$(ARTYFX_VERSION))
 ARTYFX_DEPENDENCIES = libsndfile
-ARTYFX_CONF_OPTS=-DBUILD_GUI=OFF
+ARTYFX_CONF_OPTS = -DBUILD_GUI=OFF -DBUILD_SSE=OFF
 ARTYFX_BUNDLES = artyfx.lv2
 
 define ARTYFX_POST_INSTALL_TARGET_TTLFILES
 	cp -rL $($(PKG)_PKGDIR)/artyfx.lv2/* $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/
-	# unused plugin
-	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/driva.ttl
+	# unused plugins
+	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/capta.ttl
+	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/della.ttl
+	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/ducka.ttl
+	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/friza.ttl
+	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/masha.ttl
+	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/panda.ttl
+	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/satma.ttl
+	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/vihda.ttl
+	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/whaaa.ttl
 endef
 
 ARTYFX_POST_INSTALL_TARGET_HOOKS += ARTYFX_POST_INSTALL_TARGET_TTLFILES
