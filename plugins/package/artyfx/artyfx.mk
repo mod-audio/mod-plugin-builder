424843e2aaddaefd97cafd26a1e6cec776edda2d
falkTX <falktx@falktx.com>
2021-09-15 10:16:55 +0100

    Split artyfx-bitta.lv2 bundle
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/artyfx/artyfx.mk
+++ plugins/package/artyfx/artyfx.mk
@@ -8,11 +8,12 @@ ARTYFX_VERSION = 9b147f25c5b0e45e52c10d9b148ff04e7cc24951
 ARTYFX_SITE = $(call github,harryhaaren,openAV-ArtyFX,$(ARTYFX_VERSION))
 ARTYFX_DEPENDENCIES = libsndfile
 ARTYFX_CONF_OPTS = -DBUILD_GUI=OFF -DBUILD_SSE=OFF
-ARTYFX_BUNDLES = artyfx.lv2
+ARTYFX_BUNDLES = artyfx.lv2 artyfx-bitta.lv2
 
 define ARTYFX_POST_INSTALL_TARGET_TTLFILES
 	cp -rL $($(PKG)_PKGDIR)/artyfx.lv2/* $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/
 	# unused plugins
+	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/bitta.ttl
 	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/capta.ttl
 	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/della.ttl
 	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/ducka.ttl
@@ -22,6 +23,11 @@ define ARTYFX_POST_INSTALL_TARGET_TTLFILES
 	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/satma.ttl
 	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/vihda.ttl
 	rm $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/whaaa.ttl
+
+	# bitta split bundle
+	install -d $(TARGET_DIR)/usr/lib/lv2/artyfx-bitta.lv2
+	cp $(TARGET_DIR)/usr/lib/lv2/artyfx.lv2/*.so $(TARGET_DIR)/usr/lib/lv2/artyfx-bitta.lv2/
+	cp -rL $($(PKG)_PKGDIR)/artyfx-bitta.lv2/* $(TARGET_DIR)/usr/lib/lv2/artyfx-bitta.lv2/
 endef
 
 ARTYFX_POST_INSTALL_TARGET_HOOKS += ARTYFX_POST_INSTALL_TARGET_TTLFILES
