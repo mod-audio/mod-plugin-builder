64b575ac9e7f52dc2450ec0132ea6fe0bda1ba6e
brummer10 <brummer-@web.de>
2021-12-02 15:46:23 +0100

    Add CollisionDrive

---
+++ plugins/package/collisiondrive/collisiondrive.mk
@@ -0,0 +1,25 @@
+######################################
+#
+# CollisionDrive
+#
+######################################
+
+COLLISIONDRIVE_VERSION = 84925a3378047ccf5d27e04a75b4de044007249b
+COLLISIONDRIVE_SITE = $(call github,brummer10,CollisionDrive,$(COLLISIONDRIVE_VERSION))
+COLLISIONDRIVE_BUNDLES = CollisionDrive.lv2
+
+ifdef BR2_cortex_a7
+COLLISIONDRIVE_SSE_CFLAGS = -mfpu=vfpv3
+endif
+
+COLLISIONDRIVE_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE)  SSE_CFLAGS="$(COLLISIONDRIVE_SSE_CFLAGS)" -C $(@D)
+
+define COLLISIONDRIVE_BUILD_CMDS
+	$(COLLISIONDRIVE_TARGET_MAKE) mod
+endef
+
+define COLLISIONDRIVE_INSTALL_TARGET_CMDS
+	$(COLLISIONDRIVE_TARGET_MAKE) install DESTDIR=$(TARGET_DIR) INSTALL_DIR=/usr/lib/lv2
+endef
+
+$(eval $(generic-package))
