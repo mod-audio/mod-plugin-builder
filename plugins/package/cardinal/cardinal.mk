1c1bc16eb778b8795ad5939dae86b75240369f7c
falkTX <falktx@falktx.com>
2022-01-12 16:14:33 +0000

    Add cardinal
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ plugins/package/cardinal/cardinal.mk
@@ -0,0 +1,34 @@
+######################################
+#
+# cardinal
+#
+######################################
+
+CARDINAL_VERSION = 5e5a53ab2bd93f378dd68f592dc1cef2d55a986c
+CARDINAL_SITE = https://github.com/DISTRHO/Cardinal.git
+CARDINAL_SITE_METHOD = git
+# CARDINAL_DEPENDENCIES = libsndfile
+CARDINAL_BUNDLES = CardinalFX.lv2
+
+# needed for submodules support
+CARDINAL_PRE_DOWNLOAD_HOOKS += MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES
+
+CARDINAL_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) CFLAGS=-ffat-lto-objects CXXFLAGS=-ffat-lto-objects \
+	$(MAKE) HEADLESS=true NOOPT=true WITH_LTO=true STATIC_BUILD=true -C $(@D)
+
+define CARDINAL_BUILD_CMDS
+	# fancy dance to skip building Standalone and VSTs
+	$(CARDINAL_TARGET_MAKE) deps plugins dpf/utils/lv2_ttl_generator
+	$(CARDINAL_TARGET_MAKE) -C src rack.a
+	$(CARDINAL_TARGET_MAKE) -C src/CardinalFX lv2 resources
+# 	$(CARDINAL_TARGET_MAKE) -C src/CardinalSynth lv2
+	$(CARDINAL_TARGET_MAKE) -C plugins resources
+	cd $(@D) && ./dpf/utils/generate-ttl.sh
+endef
+
+define CARDINAL_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/lib/lv2/CardinalFX.lv2
+	cp -rL $(@D)/bin/CardinalFX.lv2/* $(TARGET_DIR)/usr/lib/lv2/CardinalFX.lv2/
+endef
+
+$(eval $(generic-package))
