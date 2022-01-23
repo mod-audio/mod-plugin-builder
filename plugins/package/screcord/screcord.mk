ec1b6662ab592d46ac0b5be624a15b61c61dbd44
brummer10 <brummer-@web.de>
2022-01-23 06:38:26 +0100

    Implement leading zeros for filename numbering

--- plugins/package/screcord/screcord.mk
+++ plugins/package/screcord/screcord.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-SCRECORD_VERSION = f62e7aaa0a056ee1d3034c694c76c9e28e1d302f
+SCRECORD_VERSION = 533aae09e627c31cf102c4a6005019938e6a7f68
 SCRECORD_SITE = $(call github,brummer10,screcord.lv2,$(SCRECORD_VERSION))
 SCRECORD_BUNDLES = sc_record.lv2
 
