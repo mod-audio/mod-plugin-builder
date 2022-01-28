b9f8d61381482dba9442a83927d969e33a5c8f52
falkTX <falktx@falktx.com>
2022-01-28 14:11:07 +0000

    Update cabsim ir loader
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/mod-cabsim-ir-loader/mod-cabsim-ir-loader.mk
+++ plugins/package/mod-cabsim-ir-loader/mod-cabsim-ir-loader.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-MOD_CABSIM_IR_LOADER_VERSION = 83adbbcd1f2629615737a53af186e63e161b2fb3
+MOD_CABSIM_IR_LOADER_VERSION = e4056feccfd46dc2b4d689d6206e94a3624d0755
 MOD_CABSIM_IR_LOADER_SITE = $(call github,moddevices,mod-cabsim-IR-loader,$(MOD_CABSIM_IR_LOADER_VERSION))
 
 MOD_CABSIM_IR_LOADER_BUNDLES = cabsim-IR-loader.lv2
