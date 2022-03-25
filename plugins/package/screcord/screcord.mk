f6a9efa26b52b8c1e032334a2b00c8143f8dade3
brummer10 <brummer-@web.de>
2022-03-25 12:11:34 +0100

    Update screcord UI

--- plugins/package/screcord/screcord.mk
+++ plugins/package/screcord/screcord.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-SCRECORD_VERSION = c32444aef3022b7180aa4d7bf46ea2f6d443952e
+SCRECORD_VERSION = fe0d42d31fecde5ad7d3c05514cf2ecb57fd2495
 SCRECORD_SITE = $(call github,brummer10,screcord.lv2,$(SCRECORD_VERSION))
 SCRECORD_BUNDLES = sc_record.lv2
 
