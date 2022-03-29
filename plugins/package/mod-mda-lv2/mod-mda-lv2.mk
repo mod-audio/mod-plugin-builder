43dce471a793abe19794eeb940010f14d7be0bdd
falkTX <falktx@falktx.com>
2022-03-29 12:10:11 +0100

    Update mda
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/mod-mda-lv2/mod-mda-lv2.mk
+++ plugins/package/mod-mda-lv2/mod-mda-lv2.mk
@@ -6,7 +6,7 @@
 
 MOD_MDA_LV2_VERSION = bbd8da8b869af15c5fc99ae844591d3848d2a4a9
 MOD_MDA_LV2_SITE = $(call github,moddevices,mda-lv2,$(MOD_MDA_LV2_VERSION))
-MOD_MDA_LV2_BUNDLES = mod-mda-Ambience.lv2 mod-mda-BeatBox.lv2 mod-mda-Detune.lv2 mod-mda-DubDelay.lv2 mod-mda-DX10.lv2 mod-mda-EPiano.lv2 mod-mda-JX10.lv2 mod-mda-Leslie.lv2 mod-mda-Overdrive.lv2 mod-mda-Piano.lv2 mod-mda-RePsycho.lv2 mod-mda-RingMod.lv2 mod-mda-RoundPan.lv2 mod-mda-SubSynth.lv2 mod-mda-ThruZero.lv2 mod-mda-Vocoder.lv2
+MOD_MDA_LV2_BUNDLES = mod-mda-Ambience.lv2 mod-mda-BeatBox.lv2 mod-mda-Degrade.lv2 mod-mda-Detune.lv2 mod-mda-DubDelay.lv2 mod-mda-DX10.lv2 mod-mda-EPiano.lv2 mod-mda-JX10.lv2 mod-mda-Leslie.lv2 mod-mda-Overdrive.lv2 mod-mda-Piano.lv2 mod-mda-RePsycho.lv2 mod-mda-RingMod.lv2 mod-mda-RoundPan.lv2 mod-mda-Shepard.lv2 mod-mda-SubSynth.lv2 mod-mda-ThruZero.lv2 mod-mda-Vocoder.lv2
 
 MOD_MDA_LV2_TARGET_WAF = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(HOST_DIR)/usr/bin/python2 ./waf
 
@@ -26,6 +26,7 @@ define MOD_MDA_LV2_INSTALL_TARGET_CMDS
 	# and ship with our custom bundles
 	cp -rL $($(PKG)_PKGDIR)/mod-mda-Ambience.lv2/*  $(TARGET_DIR)/usr/lib/lv2/mod-mda-Ambience.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-mda-BeatBox.lv2/*   $(TARGET_DIR)/usr/lib/lv2/mod-mda-BeatBox.lv2/
+	cp -rL $($(PKG)_PKGDIR)/mod-mda-Degrade.lv2/*   $(TARGET_DIR)/usr/lib/lv2/mod-mda-Degrade.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-mda-Detune.lv2/*    $(TARGET_DIR)/usr/lib/lv2/mod-mda-Detune.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-mda-DubDelay.lv2/*  $(TARGET_DIR)/usr/lib/lv2/mod-mda-DubDelay.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-mda-DX10.lv2/*      $(TARGET_DIR)/usr/lib/lv2/mod-mda-DX10.lv2/
@@ -37,6 +38,7 @@ define MOD_MDA_LV2_INSTALL_TARGET_CMDS
 	cp -rL $($(PKG)_PKGDIR)/mod-mda-RePsycho.lv2/*  $(TARGET_DIR)/usr/lib/lv2/mod-mda-RePsycho.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-mda-RingMod.lv2/*   $(TARGET_DIR)/usr/lib/lv2/mod-mda-RingMod.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-mda-RoundPan.lv2/*  $(TARGET_DIR)/usr/lib/lv2/mod-mda-RoundPan.lv2/
+	cp -rL $($(PKG)_PKGDIR)/mod-mda-Shepard.lv2/*   $(TARGET_DIR)/usr/lib/lv2/mod-mda-Shepard.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-mda-SubSynth.lv2/*  $(TARGET_DIR)/usr/lib/lv2/mod-mda-SubSynth.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-mda-ThruZero.lv2/*  $(TARGET_DIR)/usr/lib/lv2/mod-mda-ThruZero.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-mda-Vocoder.lv2/*   $(TARGET_DIR)/usr/lib/lv2/mod-mda-Vocoder.lv2/
