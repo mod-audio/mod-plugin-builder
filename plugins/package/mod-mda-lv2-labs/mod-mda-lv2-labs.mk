278d137521a21e10b77315df16a300c742a3225a
falkTX <falktx@falktx.com>
2022-04-01 13:32:36 +0100

    Update mda
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/mod-mda-lv2-labs/mod-mda-lv2-labs.mk
+++ plugins/package/mod-mda-lv2-labs/mod-mda-lv2-labs.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-MOD_MDA_LV2_LABS_VERSION = 8ffaabf0016b0dd6a3d0d479e40e8720c8c2ff8f
+MOD_MDA_LV2_LABS_VERSION = 99a2131d37c07f36d11cb375d2f6033d55165c2c
 MOD_MDA_LV2_LABS_SITE = $(call github,moddevices,mda-lv2,$(MOD_MDA_LV2_LABS_VERSION))
 MOD_MDA_LV2_LABS_BUNDLES = mod-mda-Bandisto.lv2 mod-mda-Combo.lv2 mod-mda-DeEss.lv2 mod-mda-Delay.lv2 mod-mda-Dither.lv2 mod-mda-Dynamics.lv2 mod-mda-Image.lv2 mod-mda-Limiter.lv2 mod-mda-Loudness.lv2 mod-mda-MultiBand.lv2 mod-mda-RezFilter.lv2 mod-mda-Splitter.lv2 mod-mda-Stereo.lv2 mod-mda-TalkBox.lv2 mod-mda-TestTone.lv2 mod-mda-Tracker.lv2 mod-mda-Transient.lv2 mod-mda-VocInput.lv2
 
--- plugins/package/mod-mda-lv2/mod-mda-lv2.mk
+++ plugins/package/mod-mda-lv2/mod-mda-lv2.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-MOD_MDA_LV2_VERSION = 8ffaabf0016b0dd6a3d0d479e40e8720c8c2ff8f
+MOD_MDA_LV2_VERSION = 99a2131d37c07f36d11cb375d2f6033d55165c2c
 MOD_MDA_LV2_SITE = $(call github,moddevices,mda-lv2,$(MOD_MDA_LV2_VERSION))
 MOD_MDA_LV2_BUNDLES = mod-mda-Ambience.lv2 mod-mda-BeatBox.lv2 mod-mda-Degrade.lv2 mod-mda-Detune.lv2 mod-mda-DubDelay.lv2 mod-mda-DX10.lv2 mod-mda-EPiano.lv2 mod-mda-JX10.lv2 mod-mda-Leslie.lv2 mod-mda-Overdrive.lv2 mod-mda-Piano.lv2 mod-mda-RePsycho.lv2 mod-mda-RingMod.lv2 mod-mda-RoundPan.lv2 mod-mda-Shepard.lv2 mod-mda-SubSynth.lv2 mod-mda-ThruZero.lv2 mod-mda-Vocoder.lv2
 
