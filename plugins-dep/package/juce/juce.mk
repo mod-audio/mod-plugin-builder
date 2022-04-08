d784d5252782f5372f9238e055aac7404cd66188
falkTX <falktx@falktx.com>
2022-04-08 22:53:12 +0100

    Rework juce package
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins-dep/package/juce/juce.mk
+++ plugins-dep/package/juce/juce.mk
@@ -4,26 +4,39 @@
 #
 ######################################
 
-JUCE_VERSION = d10bc63cd1d9959e15f4713b15ebe0199e1ca996
+JUCE_VERSION = b8d8de99583fc03caca779cb5f47476382adbc50
 JUCE_SITE = $(call github,DISTRHO,juce,$(JUCE_VERSION))
-JUCE_DEPENDENCIES = freetype xlib_libXcursor host-cmake
-HOST_JUCE_DEPENDENCIES = host-cmake host-freetype host-xlib_libXcursor
+JUCE_DEPENDENCIES = host-cmake freetype
+HOST_JUCE_DEPENDENCIES = host-cmake host-freetype
 
-# FIXME build with those disabled
-JUCE_DEPENDENCIES += xlib_libXinerama xlib_libXrandr
+# this custom configure follows the same rules as buildroot, with these exceptions:
+# - CMAKE_FIND_ROOT_PATH_MODE_PROGRAM="ONLY" (buildroot uses "NEVER")
+# - CMAKE_MAKE_PROGRAM="/usr/bin/make" (not needed in buildroot, only for juce)
+# - PKG_CONFIG related env vars (buildroot unsets them for the build)
+define HOST_JUCE_CONFIGURE_CMDS
+	(mkdir -p $(@D) && cd $(@D) && rm -f CMakeCache.txt && \
+		env PKG_CONFIG_SYSROOT_DIR="$(HOST_DIR)" PKG_CONFIG_PATH="$(HOST_DIR)/usr/lib/pkgconfig" \
+		$(HOST_DIR)/usr/bin/cmake $(@D) \
+		-DCMAKE_INSTALL_PREFIX=$(HOST_DIR)/usr \
+		-DCMAKE_INSTALL_SO_NO_EXE=0 \
+		-DCMAKE_FIND_ROOT_PATH="$(HOST_DIR)" \
+		-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM="ONLY" \
+		-DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE="ONLY" \
+		-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE="ONLY" \
+		-DCMAKE_MAKE_PROGRAM="/usr/bin/make" \
+		-DCMAKE_C_COMPILER="$(HOSTCC)" \
+		-DCMAKE_CXX_COMPILER="$(HOSTCXX)" \
+		-DJUCE_BUILD_HELPER_TOOLS=ON \
+		-DJUCE_INSTALL_DESTINATION=lib/cmake/JUCE \
+	)
+endef
 
-# TODO actually build it
+define HOST_JUCE_POST_INSTALL_JUCEAIDE
+	$(INSTALL) -m 755 $(@D)/extras/Build/juceaide/juceaide_artefacts/juceaide $(HOST_DIR)/usr/bin/
+	$(INSTALL) -m 755 $($(PKG)_PKGDIR)/toolchainfile.cmake $(HOST_DIR)/usr/lib/cmake/JUCE/
+endef
+
+HOST_JUCE_POST_INSTALL_HOOKS += HOST_JUCE_POST_INSTALL_JUCEAIDE
 
 $(eval $(generic-package))
-$(eval $(host-generic-package))
-
-# Configure build with library components only
-# cmake -B cmake-build-install -DCMAKE_INSTALL_PREFIX=/path/to/JUCE/install
-# Run the installation
-# cmake --build cmake-build-install --target install
-
-# plugin side
-# find_package(JUCE CONFIG REQUIRED)
-
-# $(eval $(cmake-package))
-# $(eval $(host-cmake-package))
+$(eval $(host-cmake-package))
