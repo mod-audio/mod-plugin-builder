c39043dd8793487056dd79d2bdb62d30f72efbb6
falkTX <falktx@falktx.com>
2022-04-21 22:54:50 +0100

    Update chow-centaur for proper mod+juce build
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/chow-centaur/chow-centaur.mk
+++ plugins/package/chow-centaur/chow-centaur.mk
@@ -7,10 +7,8 @@
 CHOW_CENTAUR_VERSION = f3bb633a593b6fbb22a44c1ef9d1dbedbfe92d5b
 CHOW_CENTAUR_SITE = https://github.com/jatinchowdhury18/KlonCentaur.git
 CHOW_CENTAUR_SITE_METHOD = git
-CHOW_CENTAUR_DEPENDENCIES = juce host-juce
-# TODO make some jack2mod optional
-CHOW_CENTAUR_DEPENDENCIES += jack2mod
-CHOW_CENTAUR_CONF_ENV = MOD_TOOLCHAIN_FILE=$(BR2_EXTERNAL)/package/juce/toolchain.cmake
+CHOW_CENTAUR_DEPENDENCIES = juce
+CHOW_CENTAUR_CONF_OPTS = -DCMAKE_TOOLCHAIN_FILE=$(BR2_EXTERNAL)/package/juce/toolchainfile.cmake -DSKIP_LV2_TTL_GENERATOR=ON
 CHOW_CENTAUR_BUNDLES = ChowCentaur.lv2
 
 # needed for submodules support
@@ -18,8 +16,8 @@ CHOW_CENTAUR_PRE_DOWNLOAD_HOOKS += MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES
 
 define CHOW_CENTAUR_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib/lv2/ChowCentaur.lv2
-	cp -rL $($(PKG)_PKGDIR)/ChowCentaur.lv2/* $(TARGET_DIR)/usr/lib/lv2/ChowCentaur.lv2/
 	cp -rL $(@D)/ChowCentaur/ChowCentaur_artefacts/Release/LV2/ChowCentaur.lv2/* $(TARGET_DIR)/usr/lib/lv2/ChowCentaur.lv2/
+	cp -rL $($(PKG)_PKGDIR)/ChowCentaur.lv2/* $(TARGET_DIR)/usr/lib/lv2/ChowCentaur.lv2/
 endef
 
 $(eval $(cmake-package))
