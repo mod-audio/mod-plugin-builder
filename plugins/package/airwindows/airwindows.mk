d9afae033b4ee7d95a3d20d926e5c2fa10d8f0fe
falkTX <falktx@falktx.com>
2022-04-26 17:14:01 +0100

    Update airwindows, optimization over all plugins

--- plugins/package/airwindows/airwindows.mk
+++ plugins/package/airwindows/airwindows.mk
@@ -4,18 +4,18 @@
 #
 ######################################
 
-AIRWINDOWS_VERSION = 94a6cc3d6c70883e08fd7be16e9405d44bfb94e5
-AIRWINDOWS_SITE = https://code.volse.net/audio/plugins/airwindows-lv2-port.git
+AIRWINDOWS_VERSION = f175688c1b9c9285c41d1048ceef3683f32972e7
+AIRWINDOWS_SITE = https://github.com/hannesbraun/airwindows-lv2.git
 AIRWINDOWS_SITE_METHOD = git
-AIRWINDOWS_SUBDIR = plugins/LV2
-AIRWINDOWS_BUNDLES = airwindows.lv2
+AIRWINDOWS_CONF_OPTS = -DCMAKE_INSTALL_PREFIX=/usr/lib/lv2
+AIRWINDOWS_BUNDLES = Airwindows.lv2
 
 # needed for submodules support
-AIRWINDOWS_PRE_DOWNLOAD_HOOKS += MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES
+# AIRWINDOWS_PRE_DOWNLOAD_HOOKS += MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES
 
-define AIRWINDOWS_INSTALL_TARGET_CMDS
-	cp -rL $(@D)/$(AIRWINDOWS_SUBDIR)/airwindows.lv2 $(TARGET_DIR)/usr/lib/lv2/
+# define AIRWINDOWS_INSTALL_TARGET_CMDS
+# 	cp -rL $(@D)/$(AIRWINDOWS_SUBDIR)/airwindows.lv2 $(TARGET_DIR)/usr/lib/lv2/
 # 	cp -rL $($(PKG)_PKGDIR)/airwindows.lv2/*         $(TARGET_DIR)/usr/lib/lv2/airwindows.lv2/
-endef
+# endef
 
 $(eval $(cmake-package))
