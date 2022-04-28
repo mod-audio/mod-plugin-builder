231ff32bd017f974a28400986936d1b9e69aadca
falkTX <falktx@falktx.com>
2022-04-28 23:59:04 +0100

    Add x42-zconvo package
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ plugins/package/x42-zconvo/x42-zconvo.mk
@@ -0,0 +1,25 @@
+######################################
+#
+# x42-zconvo
+#
+######################################
+
+# NOTE if updating, make sure to revert 2ba6328a4b15cca07d493fa14edcf5ab367b92fc (unwanted in MOD)
+X42_ZCONVO_VERSION = 9a056680a7d460ec93ec5ff355812586513ed2ea
+X42_ZCONVO_SITE = $(call github,x42,zconvo.lv2,$(X42_ZCONVO_VERSION))
+X42_ZCONVO_DEPENDENCIES = fftw-single libsamplerate libsndfile
+X42_ZCONVO_BUNDLES = zeroconvo.lv2
+
+X42_ZCONVO_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) MOD=1 OPTIMIZATIONS="-fno-finite-math-only -DNDEBUG" PREFIX=/usr -C $(@D)
+
+define X42_ZCONVO_BUILD_CMDS
+	$(X42_ZCONVO_TARGET_MAKE)
+endef
+
+define X42_ZCONVO_INSTALL_TARGET_CMDS
+	$(X42_ZCONVO_TARGET_MAKE) install DESTDIR=$(TARGET_DIR)
+	rm -rf $(TARGET_DIR)/usr/lib/lv2/zeroconvo.lv2/ir
+	cp -rL $($(PKG)_PKGDIR)/zeroconvo.lv2/* $(TARGET_DIR)/usr/lib/lv2/zeroconvo.lv2/
+endef
+
+$(eval $(generic-package))
