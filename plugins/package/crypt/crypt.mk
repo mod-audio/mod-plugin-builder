bb711e88181c41954ef1deb847deb62864af9e1d
falkTX <falktx@falktx.com>
2022-05-13 01:00:27 +0100

    Add crypt, update dragonfly
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ plugins/package/crypt/crypt.mk
@@ -0,0 +1,21 @@
+######################################
+#
+# crypt
+#
+######################################
+
+CRYPT_VERSION = 1e0a3fd633f16e98d3013d247ba94fde04c470ef
+CRYPT_SITE = https://github.com/moddevices/crypt/
+CRYPT_SITE_METHOD = git
+CRYPT_DEPENDENCIES = juce
+CRYPT_CONF_ENV = DESTDIR="$(TARGET_DIR)"
+CRYPT_CONF_OPTS = -DCMAKE_TOOLCHAIN_FILE=$(BR2_EXTERNAL)/../plugins-dep/package/juce/toolchainfile.cmake -DSKIP_LV2_TTL_GENERATOR=ON
+CRYPT_BUNDLES = Crypt.lv2
+
+define CRYPT_POST_INSTALL_TARGET_TTL
+	cp -rL $($(PKG)_PKGDIR)/Crypt.lv2/* $(TARGET_DIR)/usr/lib/lv2/Crypt.lv2/
+endef
+
+CRYPT_POST_INSTALL_TARGET_HOOKS += CRYPT_POST_INSTALL_TARGET_TTL
+
+$(eval $(cmake-package))
---
+++ plugins/package/dragonfly-reverb/dragonfly-reverb.mk
@@ -0,0 +1,33 @@
+######################################
+#
+# dragonfly-reverb
+#
+######################################
+
+DRAGONFLY_REVERB_VERSION = 73774a742a72fefb8da59471fc10a01913b092ae
+DRAGONFLY_REVERB_SITE = https://github.com/michaelwillis/dragonfly-reverb.git
+DRAGONFLY_REVERB_SITE_METHOD = git
+DRAGONFLY_REVERB_BUNDLES = DragonflyEarlyReflections.lv2 DragonflyHallReverb.lv2 DragonflyPlateReverb.lv2 DragonflyRoomReverb.lv2
+
+# needed for submodules support
+DRAGONFLY_REVERB_PRE_DOWNLOAD_HOOKS += MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES
+
+DRAGONFLY_REVERB_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) NOOPT=true
+
+define DRAGONFLY_REVERB_BUILD_CMDS
+	$(DRAGONFLY_REVERB_TARGET_MAKE) lv2_dsp -C $(@D)/plugins/dragonfly-hall-reverb
+	$(DRAGONFLY_REVERB_TARGET_MAKE) lv2_dsp -C $(@D)/plugins/dragonfly-room-reverb
+	$(DRAGONFLY_REVERB_TARGET_MAKE) lv2_dsp -C $(@D)/plugins/dragonfly-plate-reverb
+	$(DRAGONFLY_REVERB_TARGET_MAKE) lv2_dsp -C $(@D)/plugins/dragonfly-early-reflections
+endef
+
+define DRAGONFLY_REVERB_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/lib/lv2
+	cp -r $(@D)/bin/*.lv2 $(TARGET_DIR)/usr/lib/lv2/
+	cp -rL $($(PKG)_PKGDIR)/DragonflyEarlyReflections.lv2/* $(TARGET_DIR)/usr/lib/lv2/DragonflyEarlyReflections.lv2/
+	cp -rL $($(PKG)_PKGDIR)/DragonflyHallReverb.lv2/*       $(TARGET_DIR)/usr/lib/lv2/DragonflyHallReverb.lv2/
+	cp -rL $($(PKG)_PKGDIR)/DragonflyPlateReverb.lv2/*      $(TARGET_DIR)/usr/lib/lv2/DragonflyPlateReverb.lv2/
+	cp -rL $($(PKG)_PKGDIR)/DragonflyRoomReverb.lv2/*       $(TARGET_DIR)/usr/lib/lv2/DragonflyRoomReverb.lv2/
+endef
+
+$(eval $(generic-package))
