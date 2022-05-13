7db715072835b75ad74c16a854008fb3f4980f5d
falkTX <falktx@falktx.com>
2022-05-13 01:01:08 +0100

    Update cardinal, use only prebuilt bin package
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/cardinal-bin/cardinal-bin.mk
+++ plugins/package/cardinal-bin/cardinal-bin.mk
@@ -4,15 +4,16 @@
 #
 ######################################
 
-CARDINAL_BIN_VERSION = 6795d8acba64ed3c63076e3856d19f4c653c7eed
+CARDINAL_BIN_VERSION = 34f49211e2ba16ebc782f46999160a02b2706198
 CARDINAL_BIN_SITE_METHOD = local
 CARDINAL_BIN_SITE = $($(PKG)_PKGDIR)/
-CARDINAL_BIN_BUNDLES = CardinalFX.lv2
+CARDINAL_BIN_BUNDLES = Cardinal.lv2 CardinalFX.lv2
 
 define CARDINAL_BIN_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/lib/lv2/CardinalFX.lv2
+	mkdir -p $(TARGET_DIR)/usr/lib/lv2
 	cp -r $(@D)/bundles/* $(TARGET_DIR)/usr/lib/lv2/
-	cp $(@D)/binaries/$(MOD_BIN_COMPAT)/*.so $(TARGET_DIR)/usr/lib/lv2/CardinalFX.lv2/
+	cp $(@D)/binaries/$(MOD_BIN_COMPAT)/Cardinal.so $(TARGET_DIR)/usr/lib/lv2/Cardinal.lv2/
+	cp $(@D)/binaries/$(MOD_BIN_COMPAT)/CardinalFX.so $(TARGET_DIR)/usr/lib/lv2/CardinalFX.lv2/
 endef
 
 $(eval $(generic-package))
