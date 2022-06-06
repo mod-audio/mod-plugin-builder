2943cc1ee7baaacd190d18a96b4255d62ad867de
falkTX <falktx@falktx.com>
2022-06-06 19:03:40 +0100

    Update mod-system-plugins
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/mod-system-plugins/mod-system-plugins.mk
+++ plugins/package/mod-system-plugins/mod-system-plugins.mk
@@ -1,12 +1,11 @@
 ######################################
 #
-# MOD-system-plugins
+# mod-system-plugins
 #
 ######################################
 
-MOD_SYSTEM_PLUGINS_VERSION = 34b906e019519155ff9412957b2006bc6a4483ee
+MOD_SYSTEM_PLUGINS_VERSION = b661e8f66c3448c7a7f0b440c1d02b5a553d33af
 MOD_SYSTEM_PLUGINS_SITE = $(call github,moddevices,mod-system-plugins,$(MOD_SYSTEM_PLUGINS_VERSION))
-
 MOD_SYSTEM_PLUGINS_BUNDLES = system-noisegate.lv2 advanced-noisegate.lv2 system-compressor.lv2 advanced-compressor.lv2
 
 MOD_SYSTEM_PLUGINS_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
@@ -16,7 +15,7 @@ define MOD_SYSTEM_PLUGINS_BUILD_CMDS
 endef
 
 define MOD_SYSTEM_PLUGINS_INSTALL_TARGET_CMDS
-	$(MOD_SYSTEM_PLUGINS_TARGET_MAKE) install DESTDIR=$(TARGET_DIR)
+	$(MOD_SYSTEM_PLUGINS_TARGET_MAKE) install PREFIX=/usr DESTDIR=$(TARGET_DIR)
 endef
 
 $(eval $(generic-package))
