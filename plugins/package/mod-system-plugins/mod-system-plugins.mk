d168394f76551d0f25cf9a3e83c42f572aeed1c9
falkTX <falktx@falktx.com>
2022-06-08 14:19:00 +0100

    Update mod-system-plugins
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/mod-system-plugins/mod-system-plugins.mk
+++ plugins/package/mod-system-plugins/mod-system-plugins.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-MOD_SYSTEM_PLUGINS_VERSION = b661e8f66c3448c7a7f0b440c1d02b5a553d33af
+MOD_SYSTEM_PLUGINS_VERSION = ada2b736b9b7c709c96bb5c256061af409fba920
 MOD_SYSTEM_PLUGINS_SITE = $(call github,moddevices,mod-system-plugins,$(MOD_SYSTEM_PLUGINS_VERSION))
 MOD_SYSTEM_PLUGINS_BUNDLES = system-noisegate.lv2 advanced-noisegate.lv2 system-compressor.lv2 advanced-compressor.lv2
 
