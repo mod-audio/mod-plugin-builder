e0e92fbd986448ab8c97b995b17249c0ba0cd87e
falkTX <falktx@falktx.com>
2022-06-09 12:45:40 +0100

    Update mod-cv-plugins
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/mod-cv-plugins/mod-cv-plugins.mk
+++ plugins/package/mod-cv-plugins/mod-cv-plugins.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-MOD_CV_PLUGINS_VERSION = 5b3ffb1b163d7770d993261867bad3b17db1a929
+MOD_CV_PLUGINS_VERSION = f4bd51df72ba67e9ddec47d05d5375db0125a982
 MOD_CV_PLUGINS_SITE = https://github.com/moddevices/mod-cv-plugins.git
 MOD_CV_PLUGINS_DEPENDENCIES =
 MOD_CV_PLUGINS_BUNDLES = logic-operators.lv2 mod-audio-to-cv.lv2 mod-button-to-cv.lv2 mod-cv-abs.lv2 mod-cv-attenuverter.lv2 mod-cv-clock.lv2 mod-cv-control.lv2 mod-cv-gate.lv2 mod-cv-meter.lv2 mod-cv-parameter-modulation.lv2 mod-cv-random.lv2 mod-cv-range.lv2 mod-cv-round.lv2 mod-cv-slew.lv2 mod-cv-switch1.lv2 mod-cv-switch2.lv2 mod-cv-switch3.lv2 mod-cv-switch4.lv2 mod-midi-to-cv-mono.lv2 mod-midi-to-cv-poly.lv2
@@ -26,6 +26,7 @@ define MOD_CV_PLUGINS_INSTALL_TARGET_CMDS
 	$(MOD_CV_PLUGINS_TARGET_MAKE) install DESTDIR=$(TARGET_DIR) PREFIX=/usr
 	cp -rL $($(PKG)_PKGDIR)/logic-operators.lv2/*             $(TARGET_DIR)/usr/lib/lv2/logic-operators.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-audio-to-cv.lv2/*             $(TARGET_DIR)/usr/lib/lv2/mod-audio-to-cv.lv2/
+	cp -rL $($(PKG)_PKGDIR)/mod-button-to-cv.lv2/*            $(TARGET_DIR)/usr/lib/lv2/mod-button-to-cv.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-cv-abs.lv2/*                  $(TARGET_DIR)/usr/lib/lv2/mod-cv-abs.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-cv-attenuverter.lv2/*         $(TARGET_DIR)/usr/lib/lv2/mod-cv-attenuverter.lv2/
 	cp -rL $($(PKG)_PKGDIR)/mod-cv-clock.lv2/*                $(TARGET_DIR)/usr/lib/lv2/mod-cv-clock.lv2/
