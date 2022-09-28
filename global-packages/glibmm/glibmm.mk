8f6f028855acfe44ab28aca85f8370d37b5349b1
falkTX <falktx@falktx.com>
2022-09-28 18:42:02 +0100

    Finalize details for updated dwarf toolchain
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ global-packages/glibmm/glibmm.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# glibmm
+#
+################################################################################
+
+GLIBMM_VERSION_MAJOR = 2.46
+GLIBMM_VERSION = $(GLIBMM_VERSION_MAJOR).3
+GLIBMM_LICENSE = LGPLv2.1+ (library), GPLv2+ (tools)
+GLIBMM_LICENSE_FILES = COPYING COPYING.tools
+GLIBMM_SOURCE = glibmm-$(GLIBMM_VERSION).tar.xz
+GLIBMM_SITE = http://ftp.gnome.org/pub/gnome/sources/glibmm/$(GLIBMM_VERSION_MAJOR)
+GLIBMM_INSTALL_STAGING = YES
+GLIBMM_DEPENDENCIES = libglib2 libsigc host-pkgconf
+GLIBMM_CONF_ENV = CXXFLAGS="$(TARGET_CXXFLAGS) -std=gnu++11"
+
+$(eval $(autotools-package))
