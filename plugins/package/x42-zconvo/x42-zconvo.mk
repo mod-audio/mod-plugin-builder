d1e533ab0cdd870a9bb1ff726d96cb8c835d801f
falkTX <falktx@falktx.com>
2022-10-12 13:26:35 +0100

    Update x42-zconvo
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/x42-zconvo/x42-zconvo.mk
+++ plugins/package/x42-zconvo/x42-zconvo.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-X42_ZCONVO_VERSION = 052d518ee5849f7425b89db0f791cb530100b228
+X42_ZCONVO_VERSION = bf18dc63dd1445092fb5f3af9f229b547931e532
 X42_ZCONVO_SITE = $(call github,x42,zconvo.lv2,$(X42_ZCONVO_VERSION))
 X42_ZCONVO_DEPENDENCIES = fftw-single libsamplerate libsndfile
 X42_ZCONVO_BUNDLES = zeroconvo.lv2
