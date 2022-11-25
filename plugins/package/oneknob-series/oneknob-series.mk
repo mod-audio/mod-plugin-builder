0f8cd7ad120c4758f4f6f9258aa3e788feaafcba
falkTX <falktx@falktx.com>
2022-11-25 20:10:52 +0000

    Update MOD convolution loader (renamed)
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/oneknob-series/oneknob-series.mk
+++ plugins/package/oneknob-series/oneknob-series.mk
@@ -4,15 +4,15 @@
 #
 ######################################
 
-ONEKNOB_SERIES_VERSION = 9970eed4ef80fdc7a2c66d9d7801ab23acbe5818
+ONEKNOB_SERIES_VERSION = 5867b61a8f75e9a31901fe3bc698a46b0ef4c3e0
 ONEKNOB_SERIES_SITE = https://github.com/DISTRHO/OneKnob-Series.git
 ONEKNOB_SERIES_SITE_METHOD = git
-ONEKNOB_SERIES_BUNDLES = OK-ConvolutionReverb.lv2
+ONEKNOB_SERIES_BUNDLES = MOD-ConvolutionLoader.lv2
 
 # needed for submodules support
 ONEKNOB_SERIES_PRE_DOWNLOAD_HOOKS += MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES
 
-ONEKNOB_SERIES_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) CROSS_COMPILING=true NOOPT=true WITH_LTO=true -C $(@D)
+ONEKNOB_SERIES_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) CROSS_COMPILING=true MOD_BUILD=true NOOPT=true WITH_LTO=true -C $(@D)
 
 define ONEKNOB_SERIES_BUILD_CMDS
 	$(ONEKNOB_SERIES_TARGET_MAKE)
@@ -21,7 +21,7 @@ endef
 define ONEKNOB_SERIES_INSTALL_TARGET_CMDS
 	install -d $(TARGET_DIR)/usr/lib/lv2/
 	cp -r $(@D)/bin/*.lv2 $(TARGET_DIR)/usr/lib/lv2/
-	cp -rL $($(PKG)_PKGDIR)/OK-ConvolutionReverb.lv2/* $(TARGET_DIR)/usr/lib/lv2/OK-ConvolutionReverb.lv2/
+	cp -rL $($(PKG)_PKGDIR)/MOD-ConvolutionLoader.lv2/* $(TARGET_DIR)/usr/lib/lv2/MOD-ConvolutionLoader.lv2/
 endef
 
 $(eval $(generic-package))
