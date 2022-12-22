9f3ccdc9324ec614af60b484d31ed00cfae6584f
falkTX <falktx@falktx.com>
2022-12-22 11:41:33 +0000

    Update Cardinal, Update MOD Convolution Loader description
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/cardinal-bin/cardinal-bin.mk
+++ plugins/package/cardinal-bin/cardinal-bin.mk
@@ -4,12 +4,16 @@
 #
 ######################################
 
-CARDINAL_BIN_VERSION = 22.11
+CARDINAL_BIN_VERSION = 22.12
 CARDINAL_BIN_SITE_METHOD = local
 CARDINAL_BIN_SITE = $($(PKG)_PKGDIR)/
 CARDINAL_BIN_BUNDLES = Cardinal.lv2 CardinalFX.lv2
 
 define CARDINAL_BIN_INSTALL_TARGET_CMDS
+	tar xf $(@D)/binaries.tar.xz -C $(@D) \
+		binaries/$(MOD_BIN_COMPAT)/Cardinal.so \
+		binaries/$(MOD_BIN_COMPAT)/CardinalFX.so
+
 	mkdir -p $(TARGET_DIR)/usr/lib/lv2
 	cp -r $(@D)/bundles/* $(TARGET_DIR)/usr/lib/lv2/
 	cp $(@D)/binaries/$(MOD_BIN_COMPAT)/Cardinal.so $(TARGET_DIR)/usr/lib/lv2/Cardinal.lv2/
