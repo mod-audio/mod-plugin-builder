f73345bc16e64b048aa06df7bac3218ba9094782
falkTX <falktx@falktx.com>
2023-03-06 13:15:31 +0100

    Update aidadsp-lv2
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/aidadsp-lv2/aidadsp-lv2.mk
+++ plugins/package/aidadsp-lv2/aidadsp-lv2.mk
@@ -4,10 +4,12 @@
 #
 ######################################
 
-AIDADSP_LV2_VERSION = 29a3dd1291b16da60349a9825dc3be429ba6fb52
+AIDADSP_LV2_VERSION = 0bac2f3feb811115d696b11ec2293f5f03a535ac
 AIDADSP_LV2_SITE = https://github.com/AidaDSP/aidadsp-lv2.git
 AIDADSP_LV2_SITE_METHOD = git
-AIDADSP_LV2_CONF_OPTS = -DRTNEURAL_XSIMD=ON
+# FIXME something wrong here, overoptimization?
+# AIDADSP_LV2_CONF_OPTS = -DRTNEURAL_XSIMD=ON
+AIDADSP_LV2_CONF_OPTS = -DRTNEURAL_EIGEN=ON
 AIDADSP_LV2_BUNDLES = rt-neural-generic.lv2
 
 # needed for submodules support
@@ -17,7 +19,6 @@ define AIDADSP_LV2_POST_INSTALL_TARGET_FILES
     mkdir -p $(TARGET_DIR)/usr/lib/lv2
     rm -rf $(TARGET_DIR)/usr/lib/lv2/rt-neural-generic.lv2
     mv $(TARGET_DIR)/rt-neural-generic.lv2 $(TARGET_DIR)/usr/lib/lv2/
-    cp $($(PKG)_PKGDIR)/stylesheet-rt-neural-generic.css $(TARGET_DIR)/usr/lib/lv2/rt-neural-generic.lv2/modgui
 endef
 
 AIDADSP_LV2_POST_INSTALL_TARGET_HOOKS += AIDADSP_LV2_POST_INSTALL_TARGET_FILES
