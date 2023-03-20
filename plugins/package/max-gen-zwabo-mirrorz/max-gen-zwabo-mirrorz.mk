aac3ac231624f49abbde6bd4a291b76ccd44d751
falkTX <falktx@falktx.com>
2023-03-20 17:52:31 +0100

    Add max-gen-zwabo-mirrorz
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ plugins/package/max-gen-zwabo-mirrorz/max-gen-zwabo-mirrorz.mk
@@ -0,0 +1,26 @@
+######################################
+#
+# max-gen-zwabo-mirrorz
+#
+######################################
+
+MAX_GEN_ZWABO_MIRRORZ_VERSION = 0e92714525c0334908a0eb79d6760e06316c2148
+MAX_GEN_ZWABO_MIRRORZ_SITE = https://github.com/moddevices/max-gen-plugins.git
+MAX_GEN_ZWABO_MIRRORZ_SITE_METHOD = git
+MAX_GEN_ZWABO_MIRRORZ_BUNDLES = zwabo-mirrorz.lv2
+
+# needed for submodules support
+MAX_GEN_ZWABO_MIRRORZ_PRE_DOWNLOAD_HOOKS += MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES
+
+MAX_GEN_ZWABO_MIRRORZ_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) NOOPT=true -C $(@D)
+
+define MAX_GEN_ZWABO_MIRRORZ_BUILD_CMDS
+	$(MAX_GEN_ZWABO_MIRRORZ_TARGET_MAKE)
+endef
+
+define MAX_GEN_ZWABO_MIRRORZ_INSTALL_TARGET_CMDS
+	install -d $(TARGET_DIR)/usr/lib/lv2
+	cp -r $(@D)/bin/$(MAX_GEN_ZWABO_MIRRORZ_BUNDLES) $(TARGET_DIR)/usr/lib/lv2/
+endef
+
+$(eval $(generic-package))
