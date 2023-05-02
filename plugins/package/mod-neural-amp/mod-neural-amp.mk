191e49065a35b7d44aea7dcbe84a179d4894f808
falkTX <falktx@falktx.com>
2023-05-02 18:12:09 +0200

    Add mod-neural-amp
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ plugins/package/mod-neural-amp/mod-neural-amp.mk
@@ -0,0 +1,33 @@
+######################################
+#
+# mod-neural-amp
+#
+######################################
+
+MOD_NEURAL_AMP_VERSION = 3fdf70b58692dd77832f6ebc9d87337dde600509
+MOD_NEURAL_AMP_SITE = https://github.com/moddevices/mod-neural-amp.git
+MOD_NEURAL_AMP_SITE_METHOD = git
+MOD_NEURAL_AMP_BUNDLES = mod-neural-amp.lv2
+
+# prepare custom build flags
+MOD_NEURAL_AMP_TARGET_CFLAGS = $(TARGET_CFLAGS)
+MOD_NEURAL_AMP_TARGET_CXXFLAGS = $(TARGET_CXXFLAGS) -std=gnu++17
+MOD_NEURAL_AMP_TARGET_OPTIMIZATION = -fPIC $(filter-out -funsafe-loop-optimizations,$(subst ",,$(BR2_TARGET_OPTIMIZATION)))
+
+# can't use -funsafe-loop-optimizations
+MOD_NEURAL_AMP_TARGET_CFLAGS += $(MOD_NEURAL_AMP_TARGET_OPTIMIZATION)
+MOD_NEURAL_AMP_TARGET_CXXFLAGS += $(MOD_NEURAL_AMP_TARGET_OPTIMIZATION)
+
+# LTO-specific flags (must be present on build and link stage)
+MOD_NEURAL_AMP_LTO_FLAGS = -fno-strict-aliasing -flto -ffat-lto-objects
+
+# pass options into cmake
+MOD_NEURAL_AMP_CONF_OPTS = -DPREFIX="/usr/lib/lv2"
+MOD_NEURAL_AMP_CONF_OPTS += -DCMAKE_C_FLAGS_RELEASE="$(MOD_NEURAL_AMP_TARGET_CFLAGS) $(MOD_NEURAL_AMP_LTO_FLAGS)"
+MOD_NEURAL_AMP_CONF_OPTS += -DCMAKE_CXX_FLAGS_RELEASE="$(MOD_NEURAL_AMP_TARGET_CXXFLAGS) $(MOD_NEURAL_AMP_LTO_FLAGS)"
+MOD_NEURAL_AMP_CONF_OPTS += -DCMAKE_SHARED_LINKER_FLAGS_RELEASE="$(TARGET_LDFLAGS) $(MOD_NEURAL_AMP_LTO_FLAGS)"
+
+# needed for submodules support
+MOD_NEURAL_AMP_PRE_DOWNLOAD_HOOKS += MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES
+
+$(eval $(cmake-package))
