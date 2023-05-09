2ad17abbc5a9f2558c2f70dbd50a1f6a402fcd3e
falkTX <falktx@falktx.com>
2023-05-09 19:53:55 +0200

    Update screcord
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/screcord/screcord.mk
+++ plugins/package/screcord/screcord.mk
@@ -4,14 +4,12 @@
 #
 ######################################
 
-SCRECORD_VERSION = c3eef0dbb86396df5ca72bbf122d9e79b112d527
+SCRECORD_VERSION = 0f8aa0f03988d42ae12a4a5808a14b5731bbb2af
 SCRECORD_SITE = $(call github,brummer10,screcord.lv2,$(SCRECORD_VERSION))
 SCRECORD_BUNDLES = sc_record.lv2 sc_record_mini.lv2
 
 ifdef BR2_cortex_a7
 SCRECORD_SSE_CFLAGS = -mfpu=vfpv3
-else
-SCRECORD_SSE_CFLAGS = ""
 endif
 
 SCRECORD_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) SSE_CFLAGS="$(SCRECORD_SSE_CFLAGS)" -C $(@D)
