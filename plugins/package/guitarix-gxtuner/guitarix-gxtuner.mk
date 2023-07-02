8af3991f00c444beeb0c8d26af3032a52ddabff0
falkTX <falktx@falktx.com>
2023-07-02 14:44:17 +0200

    Fix guitarix-gxtuner package
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/guitarix-gxtuner/guitarix-gxtuner.mk
+++ plugins/package/guitarix-gxtuner/guitarix-gxtuner.mk
@@ -4,28 +4,39 @@
 #
 ######################################
 
-GUITARIX_GXTUNER_VERSION = 0.43.1
-GUITARIX_GXTUNER_SITE = http://download.sourceforge.net/guitarix/
-GUITARIX_GXTUNER_SOURCE = guitarix2-$(GUITARIX_GXTUNER_VERSION).tar.xz
-GUITARIX_GXTUNER_DEPENDENCIES = boost eigen fftw-single glibmm libsndfile host-python host-intltool
+GUITARIX_GXTUNER_VERSION = d707948ced2d3ad5faca22a941db6b821fd205c5
+GUITARIX_GXTUNER_SITE = $(call github,brummer10,guitarix,$(GUITARIX_GXTUNER_VERSION))
+GUITARIX_GXTUNER_DEPENDENCIES = boost eigen ffmpeg fftw-single glibmm libsndfile host-python host-intltool
 GUITARIX_GXTUNER_BUNDLES = gxtuner.lv2
 
-GUITARIX_GXTUNER_TARGET_WAF = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) EIGEN3_PATH=$(STAGING_DIR)/usr/include/eigen3/ $(HOST_DIR)/usr/bin/python2 ./waf
+GUITARIX_GXTUNER_TARGET_WAF = \
+	$(TARGET_MAKE_ENV) \
+	$(TARGET_CONFIGURE_OPTS) \
+	EIGEN3_PATH=$(STAGING_DIR)/usr/include/eigen3/ \
+	$(HOST_DIR)/usr/bin/python ./waf
 
 define GUITARIX_GXTUNER_CONFIGURE_CMDS
-	(cd $(@D); $(GUITARIX_GXTUNER_TARGET_WAF) configure --prefix=/usr --convolver-ffmpeg --disable-sse --no-ldconfig --no-lv2-gui --no-faust)
+	(cd $(@D)/trunk; $(GUITARIX_GXTUNER_TARGET_WAF) configure \
+		--prefix=/usr \
+		--convolver-ffmpeg \
+		--disable-sse \
+		--no-faust \
+		--no-ldconfig \
+		--no-lv2-gui \
+		--no-standalone \
+	)
 endef
 
 define GUITARIX_GXTUNER_BUILD_CMDS
-	(cd $(@D); $(GUITARIX_GXTUNER_TARGET_WAF) build -j $(PARALLEL_JOBS))
+	(cd $(@D)/trunk; $(GUITARIX_GXTUNER_TARGET_WAF) build -j $(PARALLEL_JOBS))
 endef
 
 define GUITARIX_GXTUNER_INSTALL_TARGET_CMDS
 	# install to temp dir
-	(cd $(@D); $(GUITARIX_GXTUNER_TARGET_WAF) install --destdir=$(TARGET_DIR)/guitarix)
-	
+	(cd $(@D)/trunk; $(GUITARIX_GXTUNER_TARGET_WAF) install --destdir=$(TARGET_DIR)/guitarix)
+
 	# move out gxtuner
-	mv $(TARGET_DIR)/guitarix/gxtuner.lv2 $(TARGET_DIR)/usr/lib/lv2/
+	mv $(TARGET_DIR)/guitarix/usr/lib/lv2/gxtuner.lv2 $(TARGET_DIR)/usr/lib/lv2/
 
 	# delete everything else
 	rm -rf $(TARGET_DIR)/guitarix
