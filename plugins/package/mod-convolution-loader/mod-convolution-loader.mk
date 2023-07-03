878051936b5ade32dd10fc992fef8217e1b343c5
falkTX <falktx@falktx.com>
2023-07-03 17:54:33 +0200

    Update mod-convolution-loader, add cabinet loader
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/mod-convolution-loader/mod-convolution-loader.mk
+++ plugins/package/mod-convolution-loader/mod-convolution-loader.mk
@@ -4,10 +4,10 @@
 #
 ######################################
 
-MOD_CONVOLUTION_LOADER_VERSION = 20313592f64224266a5ace0640339b771f445214
+MOD_CONVOLUTION_LOADER_VERSION = a626adc3ccb18ce1d575523404c4d6eaa3b3f226
 MOD_CONVOLUTION_LOADER_SITE = https://github.com/moddevices/mod-convolution-loader.git
 MOD_CONVOLUTION_LOADER_SITE_METHOD = git
-MOD_CONVOLUTION_LOADER_BUNDLES = MOD-ConvolutionLoader.lv2
+MOD_CONVOLUTION_LOADER_BUNDLES = MOD-CabinetLoader.lv2 MOD-ConvolutionLoader.lv2
 
 # needed for submodules support
 MOD_CONVOLUTION_LOADER_PRE_DOWNLOAD_HOOKS += MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES
@@ -21,6 +21,7 @@ endef
 define MOD_CONVOLUTION_LOADER_INSTALL_TARGET_CMDS
 	install -d $(TARGET_DIR)/usr/lib/lv2/
 	cp -r $(@D)/bin/*.lv2 $(TARGET_DIR)/usr/lib/lv2/
+	cp -rL $($(PKG)_PKGDIR)/MOD-CabinetLoader.lv2/* $(TARGET_DIR)/usr/lib/lv2/MOD-CabinetLoader.lv2/
 	cp -rL $($(PKG)_PKGDIR)/MOD-ConvolutionLoader.lv2/* $(TARGET_DIR)/usr/lib/lv2/MOD-ConvolutionLoader.lv2/
 endef
 
