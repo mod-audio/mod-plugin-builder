80191c23a392ee5e75e8221fc32f87cbb33768c1
falkTX <falktx@falktx.com>
2023-08-23 09:06:58 +0200

    Update audio-bridge
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/audio-bridge/audio-bridge.mk
+++ plugins/package/audio-bridge/audio-bridge.mk
@@ -4,15 +4,14 @@
 #
 ######################################
 
-AUDIO_BRIDGE_VERSION = caca521802f892d7b6c3e6cb2989a22c81562736
+AUDIO_BRIDGE_VERSION = b85ef510dfca27e48fb7c86030b0ad854aaf63f3
 AUDIO_BRIDGE_SITE = $(call github,falkTX,audio-bridge,$(AUDIO_BRIDGE_VERSION))
 AUDIO_BRIDGE_DEPENDENCIES = jack2mod
 AUDIO_BRIDGE_BUNDLES = audio-bridge.lv2
 
 define AUDIO_BRIDGE_INSTALL_TARGET_CMDS
 	install -d $(TARGET_DIR)/usr/lib/lv2/$(AUDIO_BRIDGE_BUNDLES)
-	install -m 644 $(@D)/audio-bridge.so $(TARGET_DIR)/usr/lib/lv2/$(AUDIO_BRIDGE_BUNDLES)
-	install -m 644 $(@D)/res/*.ttl $(TARGET_DIR)/usr/lib/lv2/$(AUDIO_BRIDGE_BUNDLES)
+	install -m 644 $(@D)/$(AUDIO_BRIDGE_BUNDLES)/*.* $(TARGET_DIR)/usr/lib/lv2/$(AUDIO_BRIDGE_BUNDLES)
 endef
 
 $(eval $(cmake-package))
