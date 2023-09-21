f371014167b1d05c7befffb37d3b3f1a7190bcb9
falkTX <falktx@falktx.com>
2023-09-21 02:09:15 +0200

    Update shiro and zam plugins to new git clone setup
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/shiro-plugins/shiro-plugins.mk
+++ plugins/package/shiro-plugins/shiro-plugins.mk
@@ -5,29 +5,28 @@
 ######################################
 
 SHIRO_PLUGINS_VERSION = 60a678ca6abdb7fa10b0ac8dd87e0e41052abe78
-SHIRO_PLUGINS_SITE = $(call github,ninodewit,SHIRO-Plugins,$(SHIRO_PLUGINS_VERSION))
+SHIRO_PLUGINS_SITE = https://github.com/ninodewit/SHIRO-Plugins.git
+SHIRO_PLUGINS_SITE_METHOD = git
 SHIRO_PLUGINS_BUNDLES = Harmless.lv2 Larynx.lv2 Modulay.lv2 Shiroverb.lv2
 
 SHIRO_PLUGINS_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) NOOPT=true -C $(@D)
 
-# needed for git submodules
-define SHIRO_PLUGINS_EXTRACT_CMDS
-	rm -rf $(@D)
-	git clone https://github.com/ninodewit/SHIRO-Plugins.git $(@D)
-	git clone https://github.com/DISTRHO/DPF.git $(@D)/dpf
-	(cd $(@D) && \
-		git reset --hard $(SHIRO_PLUGINS_VERSION) && \
-		git submodule init && \
-		git submodule update)
-	touch $(@D)/.stamp_downloaded
-endef
+# needed for submodules support
+SHIRO_PLUGINS_PRE_DOWNLOAD_HOOKS += MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES
 
 define SHIRO_PLUGINS_BUILD_CMDS
 	$(SHIRO_PLUGINS_TARGET_MAKE)
 endef
 
 define SHIRO_PLUGINS_INSTALL_TARGET_CMDS
-	$(SHIRO_PLUGINS_TARGET_MAKE) install PREFIX=/usr DESTDIR=$(TARGET_DIR)
+	install -d $(TARGET_DIR)/usr/lib/lv2/Harmless.lv2
+	install -d $(TARGET_DIR)/usr/lib/lv2/Larynx.lv2
+	install -d $(TARGET_DIR)/usr/lib/lv2/Modulay.lv2
+	install -d $(TARGET_DIR)/usr/lib/lv2/Shiroverb.lv2
+	install -m 644 $(@D)/bin/Harmless.lv2/*.* $(TARGET_DIR)/usr/lib/lv2/Harmless.lv2/
+	install -m 644 $(@D)/bin/Larynx.lv2/*.* $(TARGET_DIR)/usr/lib/lv2/Larynx.lv2/
+	install -m 644 $(@D)/bin/Modulay.lv2/*.* $(TARGET_DIR)/usr/lib/lv2/Modulay.lv2/
+	install -m 644 $(@D)/bin/Shiroverb.lv2/*.* $(TARGET_DIR)/usr/lib/lv2/Shiroverb.lv2/
 	cp -rL $($(PKG)_PKGDIR)/Harmless.lv2/*  $(TARGET_DIR)/usr/lib/lv2/Harmless.lv2/
 	cp -rL $($(PKG)_PKGDIR)/Larynx.lv2/*    $(TARGET_DIR)/usr/lib/lv2/Larynx.lv2/
 	cp -rL $($(PKG)_PKGDIR)/Modulay.lv2/*   $(TARGET_DIR)/usr/lib/lv2/Modulay.lv2/
--- plugins/package/zam-plugins/zam-plugins.mk
+++ plugins/package/zam-plugins/zam-plugins.mk
@@ -5,32 +5,24 @@
 ######################################
 
 ZAM_PLUGINS_VERSION = 3501810d403fdf5c35031dc3ac65ad56c13a8d2a
-ZAM_PLUGINS_SITE = $(call github,zamaudio,zam-plugins,$(ZAM_PLUGINS_VERSION))
+ZAM_PLUGINS_SITE = https://github.com/zamaudio/zam-plugins.git
+ZAM_PLUGINS_SITE_METHOD = git
 ZAM_PLUGINS_BUNDLES = ZamComp.lv2 ZamCompX2.lv2
 
 ZAM_PLUGINS_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) NOOPT=true -C $(@D)
 
-# needed for git submodules
-define ZAM_PLUGINS_EXTRACT_CMDS
-	rm -rf $(@D)
-	git clone https://github.com/zamaudio/zam-plugins.git $(@D)
-	git clone https://github.com/DISTRHO/DPF.git $(@D)/dpf
-	(cd $(@D) && \
-		git reset --hard $(ZAM_PLUGINS_VERSION) && \
-		git submodule init && \
-		git submodule update)
-	touch $(@D)/.stamp_downloaded
-endef
+# needed for submodules support
+ZAM_PLUGINS_PRE_DOWNLOAD_HOOKS += MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES
 
 define ZAM_PLUGINS_BUILD_CMDS
 	$(ZAM_PLUGINS_TARGET_MAKE)
 endef
 
 define ZAM_PLUGINS_INSTALL_TARGET_CMDS
-	# fixes broken install
-	touch $(@D)/bin/ZamPhono
-
-	$(ZAM_PLUGINS_TARGET_MAKE) install PREFIX=/usr DESTDIR=$(TARGET_DIR)
+	install -d $(TARGET_DIR)/usr/lib/lv2/ZamComp.lv2
+	install -d $(TARGET_DIR)/usr/lib/lv2/ZamCompX2.lv2
+	install -m 644 $(@D)/bin/ZamComp.lv2/*.* $(TARGET_DIR)/usr/lib/lv2/ZamComp.lv2/
+	install -m 644 $(@D)/bin/ZamCompX2.lv2/*.* $(TARGET_DIR)/usr/lib/lv2/ZamCompX2.lv2/
 	cp -rL $($(PKG)_PKGDIR)/ZamComp.lv2/*       $(TARGET_DIR)/usr/lib/lv2/ZamComp.lv2/
 	cp -rL $($(PKG)_PKGDIR)/ZamCompX2.lv2/*     $(TARGET_DIR)/usr/lib/lv2/ZamCompX2.lv2/
 endef
