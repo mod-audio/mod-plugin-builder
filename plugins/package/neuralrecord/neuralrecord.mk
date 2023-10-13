1dd84d8bdcb68b59da3fac27c4bf3315d2563e58
falkTX <falktx@falktx.com>
2023-10-13 12:15:41 +0200

    Fix neuralrecord build
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/neuralrecord/neuralrecord.mk
+++ plugins/package/neuralrecord/neuralrecord.mk
@@ -9,25 +9,17 @@ NEURALRECORD_SITE = https://github.com/brummer10/neuralrecord.git
 NEURALRECORD_SITE_METHOD = git
 NEURALRECORD_BUNDLES = neuralrecord.lv2
 
-NEURALRECORD_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
+# needed for submodules support
+NEURALRECORD_PRE_DOWNLOAD_HOOKS += MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES
 
-# needed for git submodules
-define NEURALRECORD_EXTRACT_CMDS
-	rm -rf $(@D)
-	git clone --recursive $(NEURALRECORD_SITE) $(@D)
-	(cd $(@D) && \
-		git reset --hard $(NEURALRECORD_VERSION) && \
-		git submodule update)
-	touch $(@D)/.stamp_downloaded
-endef
-
+NEURALRECORD_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) NOOPT=true
 
 define NEURALRECORD_BUILD_CMDS
 	$(NEURALRECORD_TARGET_MAKE) mod
 endef
 
 define NEURALRECORD_INSTALL_TARGET_CMDS
-	$(NEURALRECORD_TARGET_MAKE) install-mod DESTDIR=$(TARGET_DIR) INSTALL_DIR=/usr/lib/lv2
+	$(NEURALRECORD_TARGET_MAKE) install-mod DESTDIR=$(TARGET_DIR) LV2_DIR=/usr/lib/lv2
 endef
 
 $(eval $(generic-package))
