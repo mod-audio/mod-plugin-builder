17d96cf2fdd98d09cb5cd66fe575e2b1536e41f2
rominator1983 <13437300+rominator1983@users.noreply.github.com>
2023-10-15 22:29:40 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -180,7 +180,8 @@ fi
 #######################################################################################################################
 # initial first build
 
-autoreconf "${BUILD_DIR}/host-pkgconf-0.9.12"
+# TODO: Where to put this. The directory is only existent after the call to ${BR2_MAKE}
+#autoreconf "${BUILD_DIR}/host-pkgconf-0.9.12"
 
 ${BR2_MAKE} ${BR2_PLATFORM}_defconfig
 
