0a26f578d7de19646f21a3e27eda5cf562f2fa83
falkTX <falktx@falktx.com>
2024-01-02 20:24:57 +0100

    Fix mod-arpeggiator build for mod-app macOS
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/mod-arpeggiator/mod-arpeggiator.mk
+++ plugins/package/mod-arpeggiator/mod-arpeggiator.mk
@@ -15,6 +15,10 @@ MOD_ARPEGGIATOR_DEPENDENCIES =
 MOD_ARPEGGIATOR_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) NOOPT=true HAVE_CAIRO=false HAVE_GL=false -C $(@D)
 
 define MOD_ARPEGGIATOR_BUILD_CMDS
+	# FIXME remove the use of this script, not needed
+	echo "#!/bin/sh" > $(@D)/dpf/utils/generate-vst-bundles.sh
+	echo "true" >> $(@D)/dpf/utils/generate-vst-bundles.sh
+	chmod +x $(@D)/dpf/utils/generate-vst-bundles.sh
 	$(MOD_ARPEGGIATOR_TARGET_MAKE)
 endef
 
