036c45d21e665892d294f0c56efac07d4ee69b09
falkTX <falktx@falktx.com>
2024-05-26 18:56:42 +0200

    Update audio-bridge
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/audio-bridge-lv2/audio-bridge-lv2.mk
+++ plugins/package/audio-bridge-lv2/audio-bridge-lv2.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-AUDIO_BRIDGE_LV2_VERSION = cb4776960379df820df93068938584ecdbc2bcaf
+AUDIO_BRIDGE_LV2_VERSION = da1dd76262a97d11efae07b1117f361b9c5a9034
 AUDIO_BRIDGE_LV2_SITE = $(call github,falkTX,audio-bridge,$(AUDIO_BRIDGE_LV2_VERSION))
 AUDIO_BRIDGE_LV2_DEPENDENCIES = jack2mod
 AUDIO_BRIDGE_LV2_BUNDLES = audio-bridge.lv2
