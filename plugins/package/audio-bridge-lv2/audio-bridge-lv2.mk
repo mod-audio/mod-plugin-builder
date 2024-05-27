e395657dd2e036d2310f4033a1b3e3bd8dc32edf
falkTX <falktx@falktx.com>
2024-05-27 11:11:33 +0200

    Update audio-bridge, was missing screenshot
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/audio-bridge-lv2/audio-bridge-lv2.mk
+++ plugins/package/audio-bridge-lv2/audio-bridge-lv2.mk
@@ -4,14 +4,15 @@
 #
 ######################################
 
-AUDIO_BRIDGE_LV2_VERSION = da1dd76262a97d11efae07b1117f361b9c5a9034
+AUDIO_BRIDGE_LV2_VERSION = fa480debc091111e8cbe85ef927ef619119d9492
 AUDIO_BRIDGE_LV2_SITE = $(call github,falkTX,audio-bridge,$(AUDIO_BRIDGE_LV2_VERSION))
 AUDIO_BRIDGE_LV2_DEPENDENCIES = jack2mod
 AUDIO_BRIDGE_LV2_BUNDLES = audio-bridge.lv2
 
 define AUDIO_BRIDGE_LV2_INSTALL_TARGET_CMDS
-	install -d $(TARGET_DIR)/usr/lib/lv2/$(AUDIO_BRIDGE_LV2_BUNDLES)
+	install -d $(TARGET_DIR)/usr/lib/lv2/$(AUDIO_BRIDGE_LV2_BUNDLES)/modgui
 	install -m 644 $(@D)/$(AUDIO_BRIDGE_LV2_BUNDLES)/*.* $(TARGET_DIR)/usr/lib/lv2/$(AUDIO_BRIDGE_LV2_BUNDLES)
+	install -m 644 $(@D)/$(AUDIO_BRIDGE_LV2_BUNDLES)/modgui/*.* $(TARGET_DIR)/usr/lib/lv2/$(AUDIO_BRIDGE_LV2_BUNDLES)/modgui
 endef
 
 $(eval $(cmake-package))
