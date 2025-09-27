c6539d11692e9f6fd6d80819d1bdbcde7df1535e
Dave Mollen <davemollen@gmail.com>
2025-09-27 15:11:47 +0200

    Add dm-Seq plugin

---
+++ plugins/package/dm-seq/dm-seq.mk
@@ -0,0 +1,24 @@
+######################################
+#
+# dm-seq
+#
+######################################
+
+DM_SEQ_VERSION = b755d55622c378709b817cf6afbaf30e4144e24e
+DM_SEQ_SITE = https://github.com/davemollen/dm-Seq.git
+DM_SEQ_SITE_METHOD = git
+DM_SEQ_BUNDLES = dm-Seq.lv2
+
+define DM_SEQ_BUILD_CMDS
+	rm -f $(@D)/lv2/dm-Seq.lv2/libdm_seq.so
+	(cd $(@D)/lv2 && \
+		~/.cargo/bin/cargo build $(MOD_PLUGIN_BUILDER_RUST_BUILD_FLAGS))
+endef
+
+define DM_SEQ_INSTALL_TARGET_CMDS
+	$(INSTALL) -d $(TARGET_DIR)/usr/lib/lv2
+	cp -rv $(@D)/lv2/dm-Seq.lv2 $(TARGET_DIR)/usr/lib/lv2/
+	$(INSTALL) -m 644 $(@D)/lv2/target/$(MOD_PLUGIN_BUILDER_RUST_TARGET)/release/libdm_seq.so $(TARGET_DIR)/usr/lib/lv2/dm-Seq.lv2/
+endef
+
+$(eval $(generic-package))
