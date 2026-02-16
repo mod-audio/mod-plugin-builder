c3ed6e6f1e7e8d30b442de63cbb03e803ea6d0da
falkTX <falktx@falktx.com>
2026-02-16 21:49:49 +0100

    Update cardinal
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/cardinal-bin/cardinal-bin.mk
+++ plugins/package/cardinal-bin/cardinal-bin.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-CARDINAL_BIN_VERSION = 24.12
+CARDINAL_BIN_VERSION = 26.01
 CARDINAL_BIN_SITE_METHOD = local
 CARDINAL_BIN_SITE = $($(PKG)_PKGDIR)/
 CARDINAL_BIN_BUNDLES = Cardinal.lv2 CardinalFX.lv2 CardinalMini.lv2
