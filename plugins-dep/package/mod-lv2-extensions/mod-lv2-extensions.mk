836aceb6dafe9725dd4d780daa0eda49c5701d25
falkTX <falktx@falktx.com>
2026-04-23 10:47:01 +0200

    Fix url
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins-dep/package/mod-lv2-extensions/mod-lv2-extensions.mk
+++ plugins-dep/package/mod-lv2-extensions/mod-lv2-extensions.mk
@@ -5,7 +5,7 @@
 ######################################
 
 MOD_LV2_EXTENSIONS_VERSION = f4341a6c2b2f50e2eb405b06ce19f9f0b4b1a62b
-MOD_LV2_EXTENSIONS_SITE = $(call github,moddevices,mod-lv2-extensions,$(MOD_LV2_EXTENSIONS_VERSION))
+MOD_LV2_EXTENSIONS_SITE = $(call github,mod-audio,mod-lv2-extensions,$(MOD_LV2_EXTENSIONS_VERSION))
 
 MOD_LV2_EXTENSIONS_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) PREFIX=/usr
 
