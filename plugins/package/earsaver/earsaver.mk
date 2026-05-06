f384a4f20e6ed434a5bae46a1d804aee68b53cbd
Mr Sensorium <faveflave@gmail.com>
2026-05-06 20:33:07 +1000

    Update Earsaver version and build commands

--- plugins/package/earsaver/earsaver.mk
+++ plugins/package/earsaver/earsaver.mk
@@ -3,24 +3,30 @@
 # earsaver
 #
 ######################################
-EARSAVER_VERSION = 491183f9a9ed44633ebf1d6e157344adc2ad6342
+EARSAVER_VERSION = 0ca173626fc1438d6cdf953e3c0a6ee6d63e6c53
 EARSAVER_SITE = https://github.com/sensorium/earsaver-lv2.git
 EARSAVER_SITE_METHOD = git
 EARSAVER_DEPENDENCIES = dpf hvcc
 EARSAVER_BUNDLES = EarSaver.lv2
+
 define EARSAVER_BUILD_CMDS
+	# fetch heavylib if not already present
+	test -f $(@D)/heavylib/hv.gt~.pd || \
+		git clone --depth=1 https://github.com/Wasted-Audio/heavylib.git $(@D)/heavylib
 	# create dpf-based build project
-	$(STAGING_DIR)/usr/bin/hvcc $(@D)/EarSaver.pd -n "EarSaver" -g dpf -o $(@D)/build
+	$(STAGING_DIR)/usr/bin/hvcc $(@D)/EarSaver.pd \
+		-n "EarSaver" -g dpf -o $(@D)/build \
+		-p $(@D)/heavylib
 	# symlink dpf dep into build dir
 	ln -sf $(STAGING_DIR)/usr/src/dpf $(@D)/build
 	# build dpf project
 	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) NOOPT=true -C $(@D)/build
 endef
+
 define EARSAVER_INSTALL_TARGET_CMDS
-	# install built bundles
 	cp -r $(@D)/build/bin/*.lv2 $(TARGET_DIR)/usr/lib/lv2/
-	# install custom ttl
 	cp $(@D)/EarSaver.lv2/*.ttl $(TARGET_DIR)/usr/lib/lv2/EarSaver.lv2/
 	cp -r $(@D)/EarSaver.lv2/modgui $(TARGET_DIR)/usr/lib/lv2/EarSaver.lv2/
 endef
+
 $(eval $(generic-package))
